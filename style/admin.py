7e7b91390c9729856d4d6395d4c96d8dfe0242f9
Johannes Wilm <johannes@wilm.no>
2014-03-14 17:53:17 -0700

    link document style creation to changes in admin interface

--- style/admin.py
+++ style/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django.core.management import call_command
 
 from . import models
 
+
 class DocumentStyleAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        call_command("create_document_styles")
+        try:
+            call_command("compress")
+        except:
+            pass
+        call_command("collectstatic", interactive=False)
+
+    def delete_model(self, request, obj):
+        obj.delete()
+        call_command("create_document_styles")
+        try:
+            call_command("compress")
+        except:
+            pass
+        call_command("collectstatic", interactive=False)
+
 
 admin.site.register(models.DocumentStyle, DocumentStyleAdmin)
 

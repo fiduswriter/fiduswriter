056a416eb7c6cffc05f0ddaf7f22a368bb01c7a0
Maciej Filip Szkodziński <maciej.szkodzinski@gmail.com>
2014-11-06 21:34:35 +0100

    test(document): create working Selenium tests
    Selenium tests will be needed for thorough testing of editor behavior in
    Firefox. Two simplest-possible caret tests are added in document.tests.
    
    The tests use LiveTornadoTestCase, which is Django's LiveServerTestCase
    customized to run a Tornado server, using the code earlier refactored
    from base.management.commands.runserver.
    
    Additionally, javascript helper functions are added in the newly created
    test folder. They are passed as a python string to Selenium to execute
    inside the browser.

---
+++ document/tests.py
@@ -0,0 +1,233 @@
+from collections import namedtuple
+
+from allauth.account.models import EmailAddress
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from document.models import Document
+from test.helpers import testCaretJS
+from test.testcases import LiveTornadoTestCase
+
+
+# DATA
+
+"""
+DocumentContents is a stringified exporter.js.node2Obj object.
+"""
+CONTENTS_EMPTY = ''.join([
+    '{"nn":"DIV","a":[["id", "document-contents"]],"c":[',
+        '{"nn":"P","c":[',
+            '{"nn":"BR"}',
+        ']}',
+    ']}',
+])
+
+"""
+Caret is {parent: String, node: Integer, offset: Integer}
+Represents the position of a caret in the document
+    parent is a jQuery selector uniquely identifying the parent element of the
+           caret
+    node is index of the caret conaining node inside the parent element
+    offset is position of the caret inside the node
+"""
+# not using a namedtuple, because they don't get converted properly to objects
+# when passed as javascript arguments
+Caret = dict
+
+CaretTestCase = namedtuple(
+    'CaretTestCase', [
+        'name',             # string to be used as the test method name,
+                            #   will be prepended with test_
+        'description',      # string with a short description of what is
+                            #   being tested
+        'givenContents',    # DocumentContents string
+        'givenCaret',       # position of caret at start of test
+        'givenKeys',        # keys to send to the editor
+        'expectedCaret',    # expected caret position at end of test
+    ]
+)
+
+
+# TEST MAKERS
+
+class DataCasesToTestMethodsMeta(type):
+    """
+    Generate test methods from data-driven cases.
+
+    The class needs to have a 'cases' attribute.
+
+    Each case needs to have a name and description attribute.
+    """
+    def __new__(mcs, name, bases, clsdict):
+
+        def gen_test(case):
+            def test(self):
+                self.runTest(case)
+            test.__doc__ = case.description
+
+            return test
+
+        for c_ in clsdict['cases']:
+            clsdict['test_%s' % c_.name] = gen_test(c_)
+
+        return type.__new__(mcs, name, bases, clsdict)
+
+
+# TESTS
+
+class CaretMovementTests(LiveTornadoTestCase):
+    # metaclass needed to turn data-based cases into test methods
+    __metaclass__ = DataCasesToTestMethodsMeta
+    username = 'Yeti'
+    email = 'yeti@example.com'
+    passtext = 'otter'
+
+    # each case will be passed into self.runTest
+    cases = [
+        CaretTestCase(**{
+            'name': 'leftFromStart',
+            'description': "caret doesn't go left of document start",
+            'givenContents': CONTENTS_EMPTY,
+            'givenCaret': Caret(
+                parent='#document-contents > :eq(0)',
+                node=0,
+                offset=0,
+            ),
+            'givenKeys': Keys.ARROW_LEFT,
+            'expectedCaret': Caret(
+                parent='#document-contents > :eq(0)',
+                node=0,
+                offset=0,
+            )
+        }),
+        CaretTestCase(**{
+            'name': 'rightFromEnd',
+            'description': "caret doesn't go right of document end",
+            'givenContents': CONTENTS_EMPTY,
+            'givenCaret': Caret(
+                parent='#document-contents > :eq(0)',
+                node=0,
+                offset=0,
+            ),
+            'givenKeys': Keys.ARROW_RIGHT,
+            'expectedCaret': Caret(
+                parent='#document-contents > :eq(0)',
+                node=0,
+                offset=0,
+            )
+        }),
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+        super(CaretMovementTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super(CaretMovementTests, cls).tearDownClass()
+
+    def setUp(self):
+        """Creates a dummy user in the database, and logs in the browser as that
+           user."""
+        # the DB gets cleared after each test, including the user
+        # may be possible to replace this with a fixture
+        self.user = User.objects.create(
+            username=self.username,
+            password=make_password(self.passtext),
+            is_active=True
+        )
+        self.user.save()
+
+        # avoid the unverified-email login trap
+        EmailAddress.objects.create(
+            user=self.user,
+            email=self.email,
+            verified=True,
+        ).save()
+
+        self.driver.get('%s%s' % (
+            self.live_server_url,
+            '/account/login/'
+        ))
+        (self.driver
+            .find_element_by_id('id_login')
+            .send_keys(self.user.username))
+        (self.driver
+            .find_element_by_id('id_password')
+            .send_keys(self.passtext))
+        (self.driver
+            .find_element_by_id('id_password')
+            .send_keys(Keys.RETURN))
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'user-preferences'))
+        )
+
+    def runTest(self, case):
+        doc = self.createDocument(case.givenContents)
+        self.loadDocumentEditor(doc)
+
+        self.injectHelpers()
+        self.setCaret(case.givenCaret)
+        (self.driver.find_element_by_id('document-contents')
+                    .send_keys(case.givenKeys))
+
+        # grab caret from browser and compare in python,
+        # to get more informative failure messages
+        self.assertEqual(
+            case.expectedCaret,
+            self.getCaret()
+        )
+        # test browser-side, in case caret grabbing is buggy
+        self.assertTrue(self.caretIsAt(case.expectedCaret))
+
+    def createDocument(self, contents):
+        doc = Document.objects.create(
+            owner=self.user,
+            contents=contents,
+        )
+        doc.save()
+        return doc
+
+    def loadDocumentEditor(self, doc):
+        self.driver.get("%s%s" % (
+            self.live_server_url,
+            doc.get_absolute_url()
+        ))
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'document-contents'))
+        )
+
+    def getCaret(self):
+        return self.driver.execute_script(
+            'return testCaret.getCaret(rangy.getSelection());'
+        )
+
+    def setCaret(self, caret):
+        return self.driver.execute_script(
+            'testCaret.setCaret(rangy.getSelection(), arguments[0]);',
+            caret
+        )
+
+    def caretIsAt(self, expectedCaret):
+        return self.driver.execute_script(
+            '''
+            return testCaret.caretsMatch(
+                arguments[0],
+                testCaret.getCaret(rangy.getSelection())
+            );
+            ''',
+            expectedCaret
+        )
+
+    def injectHelpers(self):
+        return self.driver.execute_script(
+            'window.testCaret = %s' % testCaretJS
+        )
---
+++ test/testcases.py
@@ -0,0 +1,175 @@
+import errno
+import os
+import socket
+import sys
+import threading
+import unittest
+from unittest import skipIf         # NOQA: Imported here for backward compatibility
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.core.handlers.wsgi import WSGIHandler
+from django.core.servers.basehttp import WSGIRequestHandler, WSGIServer
+from django.db import connections
+from django.utils import six
+from django.test.testcases import TransactionTestCase
+
+from tornado.ioloop import IOLoop
+
+from base.servers.tornado_django_hybrid import make_tornado_server
+
+
+class LiveTornadoThread(threading.Thread):
+    """
+    Thread for running a live http server while the tests are running.
+    """
+
+    def __init__(self, host, possible_ports, static_handler, connections_override=None):
+        self.host = host
+        self.port = None
+        self.possible_ports = possible_ports
+        self.is_ready = threading.Event()
+        self.error = None
+        self.static_handler = static_handler
+        self.connections_override = connections_override
+        super(LiveTornadoThread, self).__init__()
+
+    def run(self):
+        """
+        Sets up the live server and databases, and then loops over handling
+        http requests.
+        """
+        if self.connections_override:
+            # Override this thread's database connections with the ones
+            # provided by the main thread.
+            for alias, conn in self.connections_override.items():
+                connections[alias] = conn
+        try:
+            self.httpd = make_tornado_server()
+
+            # Go through the list of possible ports, hoping that we can find
+            # one that is free to use for the WSGI server.
+            for index, port in enumerate(self.possible_ports):
+                try:
+                    self.httpd.listen(int(port))
+                except socket.error as e:
+                    if (index + 1 < len(self.possible_ports) and
+                            e.errno == errno.EADDRINUSE):
+                        # This port is already in use, so we go on and try with
+                        # the next one in the list.
+                        continue
+                    else:
+                        # Either none of the given ports are free or the error
+                        # is something else than "Address already in use". So
+                        # we let that error bubble up to the main thread.
+                        raise
+                else:
+                    # A free port was found.
+                    self.port = port
+                    break
+
+            self.is_ready.set()
+            IOLoop.instance().start()
+        except Exception as e:
+            self.error = e
+            self.is_ready.set()
+
+    def terminate(self):
+        if hasattr(self, 'httpd'):
+            self.httpd.stop()
+            IOLoop.instance().stop()
+
+
+class LiveTornadoTestCase(TransactionTestCase):
+    """
+    Does basically the same as TransactionTestCase but also launches a live
+    http server in a separate thread so that the tests may use another testing
+    framework, such as Selenium for example, instead of the built-in dummy
+    client.
+    Note that it inherits from TransactionTestCase instead of TestCase because
+    the threads do not share the same transactions (unless if using in-memory
+    sqlite) and each thread needs to commit all their transactions so that the
+    other thread can see the changes.
+    """
+
+    static_handler = None
+
+    @property
+    def live_server_url(self):
+        return 'http://%s:%s' % (
+            self.server_thread.host, self.server_thread.port)
+
+    @classmethod
+    def setUpClass(cls):
+        connections_override = {}
+        for conn in connections.all():
+            # If using in-memory sqlite databases, pass the connections to
+            # the server thread.
+            if (conn.vendor == 'sqlite'
+                    and conn.settings_dict['NAME'] == ':memory:'):
+                # Explicitly enable thread-shareability for this connection
+                conn.allow_thread_sharing = True
+                connections_override[conn.alias] = conn
+
+        # Launch the live server's thread
+        specified_address = os.environ.get(
+            'DJANGO_LIVE_TEST_SERVER_ADDRESS', 'localhost:8081')
+
+        # The specified ports may be of the form '8000-8010,8080,9200-9300'
+        # i.e. a comma-separated list of ports or ranges of ports, so we break
+        # it down into a detailed list of all possible ports.
+        possible_ports = []
+        try:
+            host, port_ranges = specified_address.split(':')
+            for port_range in port_ranges.split(','):
+                # A port range can be of either form: '8000' or '8000-8010'.
+                extremes = list(map(int, port_range.split('-')))
+                assert len(extremes) in [1, 2]
+                if len(extremes) == 1:
+                    # Port range of the form '8000'
+                    possible_ports.append(extremes[0])
+                else:
+                    # Port range of the form '8000-8010'
+                    for port in range(extremes[0], extremes[1] + 1):
+                        possible_ports.append(port)
+        except Exception:
+            msg = 'Invalid address ("%s") for live server.' % specified_address
+            six.reraise(ImproperlyConfigured, ImproperlyConfigured(msg), sys.exc_info()[2])
+        cls.server_thread = LiveTornadoThread(
+            host,
+            possible_ports,
+            cls.static_handler,
+            connections_override=connections_override
+        )
+        cls.server_thread.daemon = True
+        cls.server_thread.start()
+
+        # Wait for the live server to be ready
+        cls.server_thread.is_ready.wait()
+        if cls.server_thread.error:
+            # Clean up behind ourselves, since tearDownClass won't get called in
+            # case of errors.
+            cls._tearDownClassInternal()
+            raise cls.server_thread.error
+
+        super(LiveTornadoTestCase, cls).setUpClass()
+
+    @classmethod
+    def _tearDownClassInternal(cls):
+        # There may not be a 'server_thread' attribute if setUpClass() for some
+        # reasons has raised an exception.
+        if hasattr(cls, 'server_thread'):
+            # Terminate the live server's thread
+            cls.server_thread.terminate()
+            cls.server_thread.join()
+
+        # Restore sqlite connections' non-shareability
+        for conn in connections.all():
+            if (conn.vendor == 'sqlite'
+                    and conn.settings_dict['NAME'] == ':memory:'):
+                conn.allow_thread_sharing = False
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._tearDownClassInternal()
+        super(LiveTornadoTestCase, cls).tearDownClass()

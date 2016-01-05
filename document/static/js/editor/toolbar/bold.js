91779681203164c9431300922522932f85848226
Johannes Wilm <mail@johanneswilm.org>
2016-01-05 03:52:02 +0100

    change execCommand for strong command

--- document/static/js/editor/toolbar/bold.js
+++ document/static/js/editor/toolbar/bold.js
@@ -1,4 +1,4 @@
 // inlinestyles bold
 jQuery(document).on('mousedown', '#button-bold:not(.disabled)', function () {
-    theEditor.editor.execCommand('strong');
+    theEditor.editor.execCommand('schema:strong:toggle');
 });

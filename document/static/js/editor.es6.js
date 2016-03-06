324554da8ce0f6cca6e363116bcd1e02af118618
Johannes Wilm <mail@johanneswilm.org>
2016-03-06 15:32:26 +0100

    move document/static/js/editor/es6_modules -> document/static/js/es6_modules/editor

--- document/static/js/editor.es6.js
+++ document/static/js/editor.es6.js
@@ -1,4 +1,4 @@
-import {Editor} from "./es6_modules/editor"
+import {Editor} from "./es6_modules/editor/editor"
 
 /* Create theEditor and make it available to the general namespace, so that all
 the non-editor-specific pieces of JavaScript that are not written in ES6 can get

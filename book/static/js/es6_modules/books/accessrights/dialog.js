48a521e351aa73b88e35a4f4ebf27767186dcacd
Johannes Wilm <mail@johanneswilm.org>
2016-03-22 02:14:11 +0100

    move remaining book functions to ES6, part 3

--- book/static/js/es6_modules/books/accessrights/dialog.js
+++ book/static/js/es6_modules/books/accessrights/dialog.js
@@ -1,73 +1,69 @@
 import {bookCollaboratorsTemplate, bookAccessRightOverviewTemplate} from "./templates"
 
-
 /**
 * Helper functions to deal with the book access rights dialog.
 */
 
-
 export let createAccessRightsDialog = function (bookIds) {
-        let dialogHeader = gettext('Share your book with others');
-        var book_collaborators = {}
+        let dialogHeader = gettext('Share your book with others')
+        let bookCollaborators = {}
 
-        let theAccessRights = window.theAccessRights;
+        let theAccessRights = window.theAccessRights
 
         let len = theAccessRights.length
 
-        let theTeamMembers = window.theTeamMembers;
+        let theTeamMembers = window.theTeamMembers
 
 
 
         for (let i = 0; i < len; i++) {
             if (_.include(bookIds, theAccessRights[i].book_id)) {
-                if ('undefined' == typeof (book_collaborators[
-                    theAccessRights[i].user_id])) {
-                    book_collaborators[theAccessRights[i].user_id] =
-                        theAccessRights[i];
-                    book_collaborators[theAccessRights[i].user_id].count =
-                        1;
+                if (!(theAccessRights[i].user_id in bookCollaborators)) {
+                    bookCollaborators[theAccessRights[i].user_id] = theAccessRights[i]
+                    bookCollaborators[theAccessRights[i].user_id].count = 1
+
                 } else {
-                    if (book_collaborators[theAccessRights[i].user_id].rights !=
-                        theAccessRights[i].rights);
-                    book_collaborators[theAccessRights[i].user_id].rights =
-                        'r';
-                    book_collaborators[theAccessRights[i].user_id].count +=
-                        1;
+                    if (bookCollaborators[theAccessRights[i].user_id].rights !=
+                        theAccessRights[i].rights)
+                    bookCollaborators[theAccessRights[i].user_id].rights =
+                        'r'
+                    bookCollaborators[theAccessRights[i].user_id].count +=
+                        1
                 }
             }
         }
-        book_collaborators = _.select(book_collaborators, function (obj) {
-            return obj.count == bookIds.length;
-        });
+        bookCollaborators = _.select(bookCollaborators, function (obj) {
+            return obj.count == bookIds.length
+        })
 
 
 
-        var dialogBody = bookAccessRightOverviewTemplate({
+        let dialogBody = bookAccessRightOverviewTemplate({
             'dialogHeader': dialogHeader,
             'contacts': theTeamMembers,
             'collaborators': bookCollaboratorsTemplate({
-                'collaborators': book_collaborators
+                'collaborators': bookCollaborators
             })
-        });
-        $('body').append(dialogBody);
-        var diaButtons = {};
+        })
+        jQuery('body').append(dialogBody)
+        let diaButtons = {}
         diaButtons[gettext('Submit')] = function () {
             //apply the current state to server
-            var collaborators = [],
-                rights = [];
-            $('#share-member .collaborator-tr').each(function () {
-                collaborators[collaborators.length] = $(this).attr(
-                    'data-id');
-                rights[rights.length] = $(this).attr('data-right');
-            });
+            let collaborators = [],
+                rights = []
+            jQuery('#share-member .collaborator-tr').each(function () {
+                collaborators[collaborators.length] = jQuery(this).attr(
+                    'data-id')
+                rights[rights.length] = jQuery(this).attr('data-right')
+            })
             submitAccessRight(bookIds,
-                collaborators, rights);
-            $(this).dialog('close');
-        };
-        diaButtons[gettext('Cancel')] = function () {
-            $(this).dialog("close");
+                collaborators, rights)
+            jQuery(this).dialog('close')
         }
-        $('#access-rights-dialog').dialog({
+        diaButtons[gettext('Cancel')] = function () {
+            jQuery(this).dialog("close")
+        }
+        jQuery('#access-rights-dialog').dialog({
             draggable: false,
             resizable: false,
             top: 10,
@@ -76,85 +72,85 @@ export let createAccessRightsDialog = function (bookIds) {
             modal: true,
             buttons: diaButtons,
             create: function () {
-                var $the_dialog = $(this).closest(".ui-dialog");
-                $the_dialog.find(".ui-button:first-child").addClass(
-                    "fw-button fw-dark");
-                $the_dialog.find(".ui-button:last").addClass(
-                    "fw-button fw-orange");
+                let theDialog = jQuery(this).closest(".ui-dialog")
+                theDialog.find(".ui-button:first-child").addClass(
+                    "fw-button fw-dark")
+                theDialog.find(".ui-button:last").addClass(
+                    "fw-button fw-orange")
             },
             close: function () {
-                $('#access-rights-dialog').dialog('destroy').remove();
+                jQuery('#access-rights-dialog').dialog('destroy').remove()
             }
-        });
-        $('.fw-checkable').bind('click', function () {
-            $.setCheckableLabel($(this));
-        });
-        $('#add-share-member').bind('click', function () {
-            var $selected_members = $(
-                '#my-contacts .fw-checkable.checked');
-            var selected_data = [];
-            $selected_members.each(function () {
-                var member_id = $(this).attr('data-id');
-                var $collaborator = $('#collaborator-' + member_id);
-                if (0 == $collaborator.size()) {
-                    selected_data[selected_data.length] = {
-                        'user_id': member_id,
-                        'user_name': $(this).attr('data-name'),
-                        'avatar': $(this).attr('data-avatar'),
+        })
+        jQuery('.fw-checkable').bind('click', function () {
+            $.setCheckableLabel(jQuery(this))
+        })
+        jQuery('#add-share-member').bind('click', function () {
+            let selectedMembers = jQuery(
+                '#my-contacts .fw-checkable.checked')
+            let selectedData = []
+            selectedMembers.each(function () {
+                let memberId = jQuery(this).attr('data-id')
+                let collaborator = jQuery('#collaborator-' + memberId)
+                if (0 == collaborator.size()) {
+                    selectedData[selectedData.length] = {
+                        'user_id': memberId,
+                        'user_name': jQuery(this).attr('data-name'),
+                        'avatar': jQuery(this).attr('data-avatar'),
                         'rights': 'r'
                     }
-                } else if ('d' == $collaborator.attr('data-right')) {
-                    $collaborator.removeClass('d').addClass('r').attr(
-                        'data-right', 'r');
+                } else if ('d' == collaborator.attr('data-right')) {
+                    collaborator.removeClass('d').addClass('r').attr(
+                        'data-right', 'r')
                 }
-            });
-            $('#my-contacts .checkable-label.checked').removeClass(
-                'checked');
-            $('#share-member table tbody').append(tmp_book_collaborators({
-                'collaborators': selected_data
-            }));
-            collaboratorFunctionsEvent();
-        });
-        collaboratorFunctionsEvent();
-    };
+            })
+            jQuery('#my-contacts .checkable-label.checked').removeClass(
+                'checked')
+            jQuery('#share-member table tbody').append(bookCollaboratorsTemplate({
+                'collaborators': selectedData
+            }))
+            collaboratorFunctionsEvent()
+        })
+        collaboratorFunctionsEvent()
+    }
 
 
     let collaboratorFunctionsEvent = function () {
-        $('.edit-right').unbind('click');
-        $('.edit-right').each(function () {
-            $.addDropdownBox($(this), $(this).siblings('.fw-pulldown'));
-        });
-        var $spans = $(
-            '.edit-right-wrapper .fw-pulldown-item, .delete-collaborator');
-        $spans.unbind('mousedown');
-        $spans.bind('mousedown', function () {
-            var new_right = $(this).attr('data-right');
-            $(this).closest('.collaborator-tr').attr('class',
-                'collaborator-tr ' + new_right);
-            $(this).closest('.collaborator-tr').attr('data-right',
-                new_right);
-        });
-    };
+        jQuery('.edit-right').unbind('click')
+        jQuery('.edit-right').each(function () {
+            $.addDropdownBox(jQuery(this), jQuery(this).siblings('.fw-pulldown'))
+        })
+        let spans = jQuery(
+            '.edit-right-wrapper .fw-pulldown-item, .delete-collaborator')
+        spans.unbind('mousedown')
+        spans.bind('mousedown', function () {
+            let newRight = jQuery(this).attr('data-right')
+            jQuery(this).closest('.collaborator-tr').attr('class',
+                'collaborator-tr ' + newRight)
+            jQuery(this).closest('.collaborator-tr').attr('data-right',
+                newRight)
+        })
+    }
 
     let submitAccessRight = function (books,
         collaborators, rights) {
-        var post_data = {
+        let postData = {
             'books[]': books,
             'collaborators[]': collaborators,
             'rights[]': rights
         }
         $.ajax({
             url: '/book/accessright/save/',
-            data: post_data,
+            data: postData,
             type: 'POST',
             dataType: 'json',
             success: function (response) {
-                theAccessRights = response.access_rights;
+                theAccessRights = response.access_rights
                 $.addAlert('success', gettext(
-                    'Access rights have been saved'));
+                    'Access rights have been saved'))
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                console.log(jqXHR.responseText);
+                console.log(jqXHR.responseText)
             }
-        });
-    };
+        })
+    }

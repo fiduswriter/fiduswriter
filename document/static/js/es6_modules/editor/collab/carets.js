485d85cb690b0292fe7e63e615052c0bc68f4e68
Johannes Wilm <mail@johanneswilm.org>
2016-04-25 10:46:52 -0400

    use positioned dom elements for collaborator's carets

--- document/static/js/es6_modules/editor/collab/carets.js
+++ document/static/js/es6_modules/editor/collab/carets.js
@@ -1,13 +1,34 @@
+import {UpdateScheduler} from "prosemirror/dist/ui/update"
+
 export class ModCollabCarets {
     constructor(mod) {
         mod.carets = this
         this.mod = mod
         this.markedSelectionUser = {}
-        this.init()
+        this.caretContainer = false
+        this.caretPlacementStyle =-false
+        this.setup()
+        this.bindEvents()
     }
 
-    init() {
+    setup() {
+        // Add one elements to hold dynamic CSS info about carets
+        let styleContainers = document.createElement('temp')
+        styleContainers.innerHTML = `<style type="text/css" id="caret-placement-style"></style>`
+        while (styleContainers.firstElementChild) {
+            document.head.appendChild(styleContainers.firstElementChild)
+        }
+        this.caretPlacementStyle = document.getElementById('caret-placement-style')
+        // Add one container element to hold carets
+        this.caretContainer = document.createElement('div')
+        this.caretContainer.id = 'caret-markers'
+        document.getElementById('paper-editable').appendChild(this.caretContainer)
+    }
 
+    bindEvents() {
+        let that = this
+        new UpdateScheduler(this.mod.editor.pm, "change", () => {return that.updatePositionCSS()})
+        new UpdateScheduler(this.mod.editor.mod.footnotes.fnPm, "change", () => {return that.updatePositionCSS()})
     }
 
     // Create a new caret as the current user
@@ -30,6 +51,7 @@ export class ModCollabCarets {
         let colorId = this.mod.colorIds[selectionUser.id]
         let posFrom = selectionUser.from
         let posTo = selectionUser.to
+        let posAnchor = selectionUser.anchor
         let pm = selectionUser.pm === 'pm' ? this.mod.editor.pm : this.mod.editor.mod.footnotes.fnPm
 
         // Delete an old marked range for the same session, if there is one.
@@ -39,30 +61,15 @@ export class ModCollabCarets {
         pm.mod.collab.unconfirmedMaps.forEach(function(map){
             posFrom = map.map(posFrom)
             posTo = map.map(posTo)
+            posAnchor = map.map(posAnchor)
         })
 
         let className = 'cursor-user-' + colorId
 
-        /* Workaround to allow for an empty marked range in some cases.
-         * See https://github.com/ProseMirror/prosemirror/issues/313
-         */
-        if (posFrom===posTo) {
-            let rp = pm.doc.resolve(posFrom)
-            if (rp.nodeAfter) {
-                posTo += 0.1
-            } else if (rp.nodeBefore) {
-                posFrom -= 1
-                className = 'cursor-user-right-' + colorId
-            } else {
-                console.log('nowhere to put the caret')
-                // The caret is likely in an empty paragraph.
-                // TODO: Figure out what to do.
-            }
-        }
+        let range = false
 
-        this.markedSelectionUser[selectionUser.sessionId] = {
-            pm,
-            range: pm.markRange(
+        if (posFrom !== posTo) {
+            range = pm.markRange(
                 posFrom,
                 posTo,
                 {
@@ -71,6 +78,22 @@ export class ModCollabCarets {
                 }
             )
         }
+
+        let anchorRange = pm.markRange(
+            posAnchor,
+            posAnchor,
+            {
+                removeWhenEmpty: false
+            }
+        )
+
+
+        let anchorNode = document.createElement('div')
+        anchorNode.id = 'caret-' + selectionUser.sessionId
+        anchorNode.classList.add('caret')
+        anchorNode.classList.add(className)
+        this.caretContainer.appendChild(anchorNode)
+        this.markedSelectionUser[selectionUser.sessionId] = {pm, range, anchorRange, anchorNode}
     }
 
     removeSelection(sessionId) {
@@ -79,7 +102,45 @@ export class ModCollabCarets {
             if (_.isFinite(selectionUser.range)) {
                 this.mod.editor[selectionUser.pm].removeRange(selectionUser.range)
             }
+            if (_.isFinite(selectionUser.anchorRange)) {
+                this.mod.editor[selectionUser.pm].removeRange(selectionUser.anchorRange)
+            }
+            selectionUser.anchorNode.parentNode.removeChild(selectionUser.anchorNode)
             delete this.markedSelectionUser[sessionId]
+        }
+    }
+
+    updatePositionCSS() {
+        // 1st write phase
+        let that = this
+
+        return function () {
+            // 1st read phase
+            // This phase + next write pahse are used for footnote placement,
+            // so we cannot find carets in the footnotes before the next read
+            // phase
+            return function () {
+                // 2nd write phase
+                return function () {
+                    // 2nd read phase
+                    let positionCSS = ''
+                    for (let sessionId in that.markedSelectionUser) {
+                        let selectionUser = that.markedSelectionUser[sessionId]
+                        let coords = selectionUser.pm.coordsAtPos(selectionUser.anchorRange.from)
+                        let offsets = that.caretContainer.getBoundingClientRect()
+                        let height = coords.bottom - coords.top
+                        let top = coords.top - offsets.top
+                        let left = coords.left - offsets.left
+                        positionCSS += `#caret-${sessionId} {top: ${top}px; left: ${left}px; height: ${height}px;}`
+                    }
+                    return function () {
+                        // 3rd write phase
+                        if (that.caretPlacementStyle.innerHTML !== positionCSS) {
+                            that.caretPlacementStyle.innerHTML = positionCSS
+                        }
+                    }
+                }
+            }
         }
     }
 }

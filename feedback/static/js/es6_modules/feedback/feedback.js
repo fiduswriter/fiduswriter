075cab7ffb12bc439907056edcc05450cd22a8d4
Johannes Wilm <mail@johanneswilm.org>
2016-05-31 21:32:09 +0200

    add browser check

--- feedback/static/js/es6_modules/feedback/feedback.js
+++ feedback/static/js/es6_modules/feedback/feedback.js
@@ -1,4 +1,11 @@
 import {csrfToken} from "../common/common"
+import * as bowser from "bowser/bowser"
+
+// Creates the feedback tab. The tab is meant for user feedback to the developers while FW is still in
+// a somewhat early stage. It is therefore included in a way so it's easy to remove from all the
+// templates.
+
+// This is therefore also where browser sniffing happens to prevent still unsupported browsers from logging in.
 
 export class FeedbackTab {
     constructor() {
@@ -26,6 +33,8 @@ export class FeedbackTab {
         jQuery("#feedbackbutton").click(function(){
             that.openFeedback()
         })
+        this.verifyBrowser()
+        window.bowser = bowser
     }
 
     openFeedback() {
@@ -51,5 +60,38 @@ export class FeedbackTab {
             }
           })
           return false
+    }
+
+    // Check if cookie value exists (don't worry about what it is)
+    checkCookie(name) {
+        return document.cookie.split(";").some(
+            function(cookie){
+                return cookie.split("=")[0].trim()===name
+            }
+        )
+    }
+
+    // Add a cookie variable with name set to true
+    setCookie(name) {
+        document.cookie = `${name}=true`
+    }
+
+    // Verify that we are running one of the (semi)-verified browser.
+    verifyBrowser() {
+        if (!(bowser.chrome||bowser.firefox||bowser.msedge||(bowser.safari && bowser.mac)) &! this.checkCookie('browsertest')) {
+            this.setCookie('browsertest')
+            let warning = gettext(`Please be aware that you are running a browser
+                that has not yet been tested with Fidus Writer so your mileage may vary.<br
+                We recommend using Google Chrome. Fidus Writer will currently not run on iOS.`)
+            let diaButtons = {}
+            diaButtons[gettext("OK")] = function() {
+                jQuery(this).dialog("close")
+            }
+            jQuery(`<div>${warning}</div>`).dialog({
+                modal: true,
+                title: gettext("Warning"),
+                buttons: diaButtons
+            })
+        }
     }
 }

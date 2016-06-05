ef60fa7bccb6fcca2a6017618a585fb650902572
Johannes Wilm <mail@johanneswilm.org>
2016-06-05 21:53:41 +0200

    add default for error messages to admins

--- feedback/models.py
+++ feedback/models.py
@@ -1,21 +1,3 @@
-#
-# This file is part of Fidus Writer <http://www.fiduswriter.org>
-#
-# Copyright (C) 2013 Takuto Kojima, Johannes Wilm
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
@@ -24,7 +6,6 @@ from django.conf import settings
 
 class Feedback(models.Model):
     message = models.TextField()
-
     owner = models.ForeignKey(User, blank=True, null=True)
 
     def __unicode__(self):

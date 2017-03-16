3fdf776396fefb923a3341e88b10b1740a3419e0
Johannes Wilm <mail@johanneswilm.org>
2017-03-16 13:52:55 +0100

    don't escape quotes in docx/odt export, fixes #397

--- document/static/js/es6_modules/exporter/tools/html.js
+++ document/static/js/es6_modules/exporter/tools/html.js
@@ -32,7 +32,6 @@ export let findImages = function(htmlCode) {
 
 export let escapeText = function(text) {
     return text
-        .replace(/"/g, '&quot;')
         .replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')

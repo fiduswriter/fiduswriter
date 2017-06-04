18f90adf270805c3012f1b6b069a9103c4963a74
Johannes Wilm <mail@johanneswilm.org>
2017-06-04 23:54:12 +0200

    add readme to latex output file

---
+++ document/static/js/es6_modules/exporter/latex/readme.js
@@ -0,0 +1,16 @@
+export let readMe =
+`In order to compile the latex file, you need to use at least TeXLive 2016.
+If there are citations, you additionally need Biber 2.7/BibLaTeX 3.7.
+
+Extract all the files included in this ZIP into a folder.
+Run then these commands to create a PDF from within this folder:
+
+> lualatex document
+
+If there are citations, continue with these commands:
+
+> biber document
+> lualatex document
+
+Look at the output messages to determine whether you need to run laluatex again.
+`

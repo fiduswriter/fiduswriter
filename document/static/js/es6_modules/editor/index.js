845322be1280e999a2224cf0068d8bb76560f0ca
Johannes Wilm <mail@johanneswilm.org>
2017-06-08 00:24:43 +0200

    3.2.0-rc.6: cleanup in moving comments when text is being deleted.

--- document/static/js/es6_modules/editor/index.js
+++ document/static/js/es6_modules/editor/index.js
@@ -593,11 +593,12 @@ export class Editor {
         }
         if (local && commentIds.length > 0) {
             // Check if the deleted comment referrers still are somewhere else in the doc.
-            // If not, delete them.
+            // If not, move them.
+            () => this.mod.comments.store.checkAndMove(commentIds)
             // TODO: Is a timeout/scheduleDOMUpdate really needed here?
-            this.pm.scheduleDOMUpdate(
-                () => this.mod.comments.store.checkAndDelete(commentIds)
-            )
+            //this.pm.scheduleDOMUpdate(
+            //
+            //)
         }
 
     }

34bd1c1d0e9604b3e36a780fa2b1e8293d5f8c37
Johannes Wilm <mail@johanneswilm.org>
2017-08-07 10:47:52 +0200

    tables plugin: make row options show upon click

--- document/static/js/es6_modules/editor/plugins/tables.js
+++ document/static/js/es6_modules/editor/plugins/tables.js
@@ -19,83 +19,84 @@ export let tablesPlugin = function(options) {
 
                     let startPos = state.selection.$head.start()
                     let tableDropUp = document.createElement('span')
-
                      tableDropUp.innerHTML = noSpaceTmp`
-                        <div class="table-edit-button-group">
-                                  <span class="ui-button-text" title="` + gettext("Insert row")+ `">
-                                        <label class="row">` + gettext("Insert row ")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Insert row before")+ `" style="display:none;">
-                                        <label class="row-before">` + gettext("Insert row before")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Insert row after")+ `">
-                                        <label class="row-after">` + gettext("Insert row after")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Insert Column before")+ `">
-                                        <label class="col-before">` + gettext("Insert Column before")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Insert Column before")+ `">
-                                        <label class="col-after">` + gettext("Insert Column after")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Delete Column")+ `">
-                                        <label class="del-col">` + gettext("Insert Column after")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Delete Row")+ `">
-                                        <label class="del-row">` + gettext("Delete Row")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Toggle Row Header")+ `">
-                                        <label class="toggle-row">` + gettext("Toggle Row Header")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Toggle Column Header")+ `">
-                                        <label class="toggle-col">` + gettext("Toggle Column Header")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Toggle Row Header")+ `">
-                                        <label class="toggle-row">` + gettext("Toggle Row Header")+ `</label>
-                                  </span>
-                                  <span class="ui-button-text" title="` + gettext("Toggle Header Cell")+ `">
-                                        <label class="toggle-cell">` + gettext("Toggle cell Header")+ `</label>
-                                  </span>
-
-
-                        </div>
+                        <span class="table-edit-button ui-button-text"><i class="icon-table"></i></span>
+                        <span class="ui-button-text row" title="${gettext("Insert row")}" style="display:none;">
+                            <label>${gettext("Insert row ")}</label>
+                        </span>
+                        <span class="ui-button-text row-before" title="${gettext("Insert row before")}" style="display:none;">
+                            <label>${gettext("Insert row before")}</label>
+                        </span>
+                        <span class="ui-button-text row-after" title="${gettext("Insert row after")}" style="display:none;">
+                            <label>${gettext("Insert row after")}</label>
+                        </span>
+                        <span class="ui-button-text" title="${gettext("Insert Column before")}">
+                            <label class="col-before">${gettext("Insert Column before")}</label>
+                        </span>
+                        <span class="ui-button-text" title="${gettext("Insert Column before")}">
+                            <label class="col-after">${gettext("Insert Column after")}</label>
+                        </span>
+                        <span class="ui-button-text" title="${gettext("Delete Column")}">
+                            <label class="del-col">${gettext("Insert Column after")}</label>
+                        </span>
+                        <span class="ui-button-text" title="${gettext("Delete Row")}">
+                            <label class="del-row">${gettext("Delete Row")}</label>
+                        </span>
+                        <span class="ui-button-text" title="${gettext("Toggle Row Header")}">
+                            <label class="toggle-row">${gettext("Toggle Row Header")}</label>
+                        </span>
+                        <span class="ui-button-text" title="${gettext("Toggle Column Header")}">
+                            <label class="toggle-col">${gettext("Toggle Column Header")}</label>
+                        </span>
+                        <span class="ui-button-text" title="${gettext("Toggle Row Header")}">
+                            <label class="toggle-row">${gettext("Toggle Row Header")}</label>
+                        </span>
+                        <span class="ui-button-text" title="${gettext("Toggle Header Cell")}">
+                            <label class="toggle-cell">${gettext("Toggle cell Header")}</label>
+                        </span>
                     `
-                    tableDropUp.querySelector('.table-edit-button-group').addEventListener('click', () => {
-                        tableDropUp.querySelector('.row').addEventListener('click', () => {
-                            tableDropUp.querySelector('.row-before').style.display = "block";
-                            tableDropUp.querySelector('.row-after').style.display = "block";
-                        })
+                    tableDropUp.querySelector('.table-edit-button').addEventListener('click', event => {
+                        tableDropUp.querySelector('.row').style.display = "block";
+                    })
+                    tableDropUp.querySelector('.row').addEventListener('click', () => {
+                        tableDropUp.querySelector('.row-before').style.display = "block";
+                        tableDropUp.querySelector('.row-after').style.display = "block";
+                    })
 
-                        tableDropUp.querySelector('.row-before').addEventListener('click', () => {
-                            addRowBefore(state, options.editor.view.dispatch)
-                        })
-                        tableDropUp.querySelector('.row-after').addEventListener('click', () => {
-                            addRowAfter(state, options.editor.view.dispatch)
-                        })
-                        tableDropUp.querySelector('.col-before').addEventListener('click', () => {
-                            addColumnBefore(state, options.editor.view.dispatch)
-                        })
-                        tableDropUp.querySelector('.col-after').addEventListener('click', () => {
-                            addColumnBefore(state, options.editor.view.dispatch)
-                        })
-                        tableDropUp.querySelector('.del-row').addEventListener('click', () => {
-                            deleteRow(state, options.editor.view.dispatch)
-                        })
-                        tableDropUp.querySelector('.del-col').addEventListener('click', () => {
-                            deleteColumn(state, options.editor.view.dispatch)
-                        })
-                        tableDropUp.querySelector('.toggle-col').addEventListener('click', () => {
-                            toggleHeaderColumn(state, options.editor.view.dispatch)
-                        })
-                        tableDropUp.querySelector('.toggle-row').addEventListener('click', () => {
-                            toggleHeaderRow(state, options.editor.view.dispatch)
-                        })
-                        tableDropUp.querySelector('.toggle-cell').addEventListener('click', () => {
-                            toggleHeaderCell(state, options.editor.view.dispatch)
-                        })
+                    tableDropUp.querySelector('.row-before').addEventListener('click', () => {
+                        addRowBefore(state, options.editor.view.dispatch)
+                    })
+                    tableDropUp.querySelector('.row-after').addEventListener('click', () => {
+                        addRowAfter(state, options.editor.view.dispatch)
+                    })
+                    tableDropUp.querySelector('.col-before').addEventListener('click', () => {
+                        addColumnBefore(state, options.editor.view.dispatch)
+                    })
+                    tableDropUp.querySelector('.col-after').addEventListener('click', () => {
+                        addColumnBefore(state, options.editor.view.dispatch)
+                    })
+                    tableDropUp.querySelector('.del-row').addEventListener('click', () => {
+                        deleteRow(state, options.editor.view.dispatch)
+                    })
+                    tableDropUp.querySelector('.del-col').addEventListener('click', () => {
+                        deleteColumn(state, options.editor.view.dispatch)
+                    })
+                    tableDropUp.querySelector('.toggle-col').addEventListener('click', () => {
+                        toggleHeaderColumn(state, options.editor.view.dispatch)
+                    })
+                    tableDropUp.querySelector('.toggle-row').addEventListener('click', () => {
+                        toggleHeaderRow(state, options.editor.view.dispatch)
+                    })
+                    tableDropUp.querySelector('.toggle-cell').addEventListener('click', () => {
+                        toggleHeaderCell(state, options.editor.view.dispatch)
                     })
 
 
-                    let deco = Decoration.widget(startPos, tableDropUp)
+                    let deco = Decoration.widget(
+                        startPos,
+                        tableDropUp,
+                        {stopEvent: event => true}
+                    )
 
                     return DecorationSet.create(state.doc, [deco])
                }

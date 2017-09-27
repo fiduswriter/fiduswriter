ff515c340d7f9d30543da82da34cb016b2438988
Johannes Wilm <mail@johanneswilm.org>
2017-09-27 10:28:15 +0200

    3.3.0-rc.10: enable error logging also when debug is true

--- fiduswriter/settings.py
+++ fiduswriter/settings.py
@@ -296,7 +296,6 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
         'console': {

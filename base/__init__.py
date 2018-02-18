9de73040cabd29e5f5a6fcb4a40046e6c2490e0f
Johannes Wilm <mail@johanneswilm.org>
2018-02-18 08:17:49 +0100

    test-caret.mjs > test-caret.js

--- base/__init__.py
+++ base/__init__.py
@@ -3,3 +3,5 @@ Contains base templates (html),
 statics that are shared among different django applications
 Also contains helper for starting tornado server and management commands
 """
+
+default_app_config = 'base.apps.BaseConfig'

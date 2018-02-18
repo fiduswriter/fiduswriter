de2347bcc999326a6c7567421ec70ccabf9d9f01
Johannes Wilm <mail@johanneswilm.org>
2018-02-18 13:35:00 +0100

    flake8

--- base/apps.py
+++ base/apps.py
@@ -6,8 +6,10 @@ from django.core.management import call_command
 
 from npm_mjs.signals import post_npm_install
 
+
 def bundle_katex(sender, **kwargs):
     call_command("bundle_katex")
+
 
 class BaseConfig(AppConfig):
     name = 'base'

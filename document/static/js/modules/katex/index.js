1105acdf18e2246e3ff66574e29fc98c2f88b018
Johannes Wilm <mail@johanneswilm.org>
2018-02-19 18:39:01 +0100

    fix katex import in a way that works for both browser and rollup

--- document/static/js/modules/katex/index.js
+++ document/static/js/modules/katex/index.js
@@ -1,8 +1,8 @@
-import {render} from "katex"
+import katex from "katex"
 
 export function katexRender (formula, node, options) {
     try {
-        render(formula, node, options)
+        katex.render(formula, node, options)
     } catch (error) {
         if (error.name === 'ParseError') {
             node.innerHTML = gettext('Formula Error!')

eb9d19ef3c3cde465f80cba59e55bc0916be89f1
Johannes Wilm <mail@johanneswilm.org>
2018-03-12 20:01:42 +0100

    only add steps

--- document/static/js/modules/editor/state_plugins/track.js
+++ document/static/js/modules/editor/state_plugins/track.js
@@ -44,6 +44,19 @@ export let trackPlugin = function(options) {
                                 tr.mapping.maps[index].map(step.to, 1)
                             ]
                         )
+                    } else if (step instanceof ReplaceAroundStep && !step.structure) {
+                        addedRanges.push(
+                            [
+                                tr.mapping.maps[index].map(step.from, -1),
+                                tr.mapping.maps[index].map(step.gapFrom, 1)
+                            ]
+                        )
+                        addedRanges.push(
+                            [
+                                tr.mapping.maps[index].map(step.gapTo, -1),
+                                tr.mapping.maps[index].map(step.to, 1)
+                            ]
+                        )
                     }
                 })
             })
@@ -56,6 +69,110 @@ export let trackPlugin = function(options) {
                 user = options.editor.user.id,
                 username = options.editor.user.username,
                 approved = !options.editor.view.state.doc.firstChild.attrs.track
+
+            if (!approved) { // Only add deletions if changes are not automatically approved
+                let deletedRanges = addedRanges.slice()
+                trs.slice().reverse().forEach(tr => {
+                    tr.steps.slice().reverse().forEach((step, index) => {
+                        let invertedStep = step.invert(tr.docs[tr.docs.length-index-1])
+                        newTr.step(invertedStep)
+                        let stepMap = invertedStep.getMap()
+                        deletedRanges = deletedRanges.map(region => [stepMap.map(region[0], -1), stepMap.map(region[1], 1)])
+                    })
+                })
+
+                let realDeletedRanges = [] // ranges of content by the same user. Should not be marked as gone, but really be removed
+
+                deletedRanges.forEach(delRange => {
+                    newTr.maybeStep(
+                        new AddMarkStep(
+                            delRange[0],
+                            delRange[1],
+                            newState.schema.marks.deletion.create({user, username, date})
+                        )
+                    )
+                    newTr.doc.nodesBetween(
+                        delRange[0],
+                        delRange[1],
+                        (node, pos, parent, index) => {
+                            if (pos < delRange[0]) {
+                                return true
+                            }
+                            if (node.marks && node.marks.find(mark => mark.type.name==='insertion' && mark.attrs.user===user && !mark.attrs.approved)) {
+                                realDeletedRanges.push([pos, pos + node.nodeSize])
+                            }
+                        }
+                    )
+                })
+                let map = new Mapping()
+                while (realDeletedRanges.length) { // We delete nodes deleted and previously inserted by same user (unapproved content)
+                    let delRange = realDeletedRanges.pop(),
+                        delStep = new ReplaceStep(
+                            delRange[0],
+                            delRange[1],
+                            Slice.empty
+                        )
+                    if (!newTr.maybeStep(delStep).failed) {
+                        let stepMap = delStep.getMap()
+                        map.appendMap(stepMap)
+                        realDeletedRanges = realDeletedRanges.map(delRange => [stepMap.map(delRange[0], -1), stepMap.map(delRange[1], 1)])
+                    }
+                }
+                addedRanges = [] // We reset the added ranges.
+
+                trs.forEach(tr => { // We insert all the same steps, but with "from"/"to" both set to "to" in order not to delete content. Mapped as needed.
+                    tr.steps.forEach((step, index) => {
+                        if (step instanceof ReplaceStep || (step instanceof ReplaceAroundStep && !step.structure)) {
+                            if (step.slice.size) {
+                                let newStep = new ReplaceStep(
+                                    map.map(step.to),
+                                    map.map(step.to),
+                                    step.slice,
+                                    step.structure
+                                )
+
+                                if (!newTr.maybeStep(newStep).failed) {
+                                    addedRanges.push(
+                                        [
+                                            map.map(step.to, -1),
+                                            map.map(step.to, 1)
+                                        ]
+                                    )
+                                    let stepMap = newStep.getMap()
+                                    addedRanges = addedRanges.map(range => [stepMap.map(range[0], -1), stepMap.map(range[1], 1)])
+                                    map.appendMap(stepMap)
+                                }
+                            }
+                        } else {
+                            let mappedStep = step.map(map)
+                            if (mappedStep) {
+                                if (!newTr.maybeStep(mappedStep).failed) {
+                                    let stepMap = mappedStep.getMap()
+                                    addedRanges = addedRanges.map(range => [stepMap.map(range[0], -1), stepMap.map(range[1], 1)])
+                                    map.appendMap(stepMap)
+                                }
+                            }
+                        }
+                        let newMap = new Mapping()
+                        newMap.appendMap(step.getMap().invert())
+                        newMap.appendMapping(map)
+                        map = newMap
+                    })
+                })
+
+                let tr = trs[trs.length-1]
+                if (tr.selection instanceof TextSelection) {
+                    let assoc = (tr.selection.from < oldState.selection.from || tr.getMeta('inputType') === 'deleteContentBackward' ) ? -1 : 1
+                    let caretPos = map.map(tr.selection.from, assoc)
+                    newTr.setSelection(
+                        new TextSelection(
+                            newTr.doc.resolve(
+                                caretPos
+                            )
+                        )
+                    )
+                }
+            }
 
             addedRanges.forEach(addedRange => {
                 newTr.maybeStep(
@@ -75,193 +192,6 @@ export let trackPlugin = function(options) {
                 return newTr
             }
 
-        },
-        filterTransaction(tr, state) {
-            if (
-                !options.editor.view.state.doc.firstChild.attrs.track || // tracking turned off. Allow.
-                ['historyUndo', 'historyRedo'].includes(tr.getMeta('inputType')) // part of history. Allow.
-            ) {
-                return true
-            }
-            // We filter to not allow deletions. Instead we mark the area that was deleted and set
-            // an insertion transaction with a timeout zero to insert the content.
-            if(
-                !tr.steps.find(step =>
-                    (
-                        step instanceof ReplaceStep && step.from !== step.to ||
-                        step instanceof ReplaceAroundStep && !step.structure
-                    )
-                ) ||
-                tr.getMeta('fromFootnote') ||
-                tr.getMeta('remote') ||
-                tr.getMeta('track')
-            ) {
-                return true
-            }
-
-            let newTr = state.tr,
-                map = new Mapping(),
-                date = Math.floor((Date.now()-options.editor.clientTimeAdjustment)/600000), // 10 minute interval
-                user = options.editor.user.id,
-                username = options.editor.user.username
-
-            newTr.setMeta('track', true)
-
-            tr.steps.forEach((step, index) => {
-
-                if (step instanceof ReplaceStep && step.from !== step.to) {
-                    newTr.maybeStep(
-                        new AddMarkStep(
-                            map.map(step.from),
-                            map.map(step.to),
-                            state.schema.marks.deletion.create({user, username, date})
-                        )
-                    )
-                    if (newTr.docs.length) {
-                        let delRanges = []
-                        newTr.doc.nodesBetween(
-                            map.map(step.from),
-                            map.map(step.to),
-                            (node, pos, parent, index) => {
-                                if (pos < map.map(step.from)) {
-                                    return true
-                                }
-                                if (node.marks && node.marks.find(mark => mark.type.name==='insertion' && mark.attrs.user===user && !mark.attrs.approved)) {
-                                    delRanges.push([pos, pos + node.nodeSize])
-                                }
-                            }
-                        )
-                        while (delRanges.length) {
-                            let delRange = delRanges.pop(),
-                                delStep = new ReplaceStep(
-                                    delRange[0],
-                                    delRange[1],
-                                    Slice.empty
-                                )
-                            let {failed} = newTr.maybeStep(delStep)
-                            if (!failed) {
-                                let stepMap = delStep.getMap()
-                                map.appendMap(stepMap)
-                                delRanges = delRanges.map(delRange => [stepMap.map(delRange[0], -1), stepMap.map(delRange[1], 1)])
-                            }
-                        }
-                    }
-
-                    if (step.slice.size) {
-                        let newStep = new ReplaceStep(
-                            map.map(step.to),
-                            map.map(step.to),
-                            step.slice,
-                            step.structure
-                        )
-
-                        let {failed} = newTr.maybeStep(newStep)
-
-                        if (!failed) {
-                            map.appendMap(newStep.getMap())
-                        }
-                    }
-
-                } else if (step instanceof ReplaceAroundStep && !step.structure) {
-                    let delRanges = []
-                    if (step.gapFrom-step.from > 0) {
-                        newTr.maybeStep(
-                            new AddMarkStep(
-                                map.map(step.from),
-                                map.map(step.gapFrom),
-                                state.schema.marks.deletion.create({user, username, date})
-                            )
-                        )
-                    }
-                    if (step.to-step.gapTo > 0) {
-                        newTr.maybeStep(
-                            new AddMarkStep(
-                                map.map(step.gapTo),
-                                map.map(step.to),
-                                state.schema.marks.deletion.create({user, username, date})
-                            )
-                        )
-                    }
-                    if (newTr.docs.length) {
-                        let delRanges = []
-                        newTr.doc.nodesBetween(
-                            map.map(step.from),
-                            map.map(step.gapFrom),
-                            (node, pos, parent, index) => {
-                                if (pos < map.map(step.from)) {
-                                    return true
-                                }
-                                if (node.marks && node.marks.find(mark => mark.type.name==='insertion' && mark.attrs.user===user && !mark.attrs.approved)) {
-                                    delRanges.push([pos, pos + node.nodeSize])
-                                }
-                            }
-                        )
-                        newTr.doc.nodesBetween(
-                            map.map(step.gapTo),
-                            map.map(step.to),
-                            (node, pos, parent, index) => {
-                                if (pos < map.map(step.gapTo)) {
-                                    return true
-                                }
-                                if (node.marks && node.marks.find(mark => mark.type.name==='insertion' && mark.attrs.user===user && !mark.attrs.approved)) {
-                                    delRanges.push([pos, pos + node.nodeSize])
-                                }
-                            }
-                        )
-                        while (delRanges.length) {
-                            let delRange = delRanges.pop(),
-                                delStep = new ReplaceStep(
-                                    delRange[0],
-                                    delRange[1],
-                                    Slice.empty
-                                )
-                            let {failed} = newTr.maybeStep(delStep)
-                            if (!failed) {
-                                let stepMap = delStep.getMap()
-                                map.appendMap(stepMap)
-                                delRanges = delRanges.map(delRange => [stepMap.map(delRange[0], -1), stepMap.map(delRange[1], 1)])
-                            }
-                        }
-                    }
-                    if (step.slice.size) {
-                        let newStep = new ReplaceStep(
-                            map.map(step.to),
-                            map.map(step.to),
-                            step.slice,
-                            step.structure
-                        )
-                        let {failed} = newTr.maybeStep(newStep)
-                        if (!failed) {
-                            map.appendMap(newStep.getMap())
-                        }
-                    }
-                } else {
-                    let mappedStep = step.map(map)
-                    if (mappedStep) {
-                        let {failed} = newTr.maybeStep(mappedStep)
-                        if (!failed) {
-                            map.appendMap(mappedStep.getMap())
-                        }
-                    }
-                }
-                let newMap = new Mapping()
-                newMap.appendMap(step.getMap().invert())
-                newMap.appendMapping(map)
-                map = newMap
-            })
-            if (tr.selection instanceof TextSelection) {
-                let assoc = (tr.selection.from < state.selection.from || tr.getMeta('inputType') === 'deleteContentBackward' ) ? -1 : 1
-                let caretPos = map.map(tr.selection.from, assoc)
-                newTr.setSelection(
-                    new TextSelection(
-                        newTr.doc.resolve(
-                            caretPos
-                        )
-                    )
-                )
-            }
-            setTimeout(() => {options.editor.view.dispatch(newTr)},0)
-            return false
         }
     })
 }

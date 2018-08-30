5860e6583ec353a3adbe36d97f452cde5afc754a
Johannes Wilm <mail@johanneswilm.org>
2018-08-30 22:20:57 +0200

    switch to vivliostyle as its AGPL does not apply to FW internals (see email from Trim-Marks), resolves #124
    Former-commit-id: fd961c72368257decd35d734121f81acfd9999d4

--- document/static/js/modules/editor/tools/print.js
+++ document/static/js/modules/editor/tools/print.js
@@ -1,48 +1,80 @@
-import {PaginateForPrint} from "paginate-for-print/dist/paginate-for-print"
+import {viewer} from "vivliostyle"
 
-const PAPER_HEIGHTS = {
-    'A4': 1117,
-    'US Letter': 1020
+const CSS_PAPER_SIZES = {
+    'A4': 'A4',
+    'US Letter': 'letter'
 }
+
+const printTemplate = ({HTML, styleSheets}) =>
+`<!DOCTYPE html>
+<html>
+    <head>
+        ${styleSheets.map(styleSheet => `
+            <style>
+                ${styleSheet}
+            </style>`
+        ).join('')}
+    </head>
+    <body>
+        ${HTML}
+    </body>
+</html>`
 
 export class ModToolsPrint {
     constructor(mod) {
         mod.print = this
         this.mod = mod
         this.paginator = false
+        this.hiddenElements = []
         this.initFlowTo()
+        window.viewer = viewer
     }
 
     initFlowTo() {
         this.flowTo = document.createElement('div')
-        this.flowTo.classList.add('print')
-        this.hideFlowTo()
+        this.flowTo.id = 'print'
         document.body.appendChild(this.flowTo)
+        this.hideFlowTo()
     }
 
     showFlowTo() {
+        Array.from(document.body.children).forEach(node => {
+            if (node !== this.flowTo) {
+                node.style.display='none'
+                this.hiddenElements.push(node)
+            }
+        })
         this.flowTo.style.display = ''
     }
 
     hideFlowTo() {
         this.flowTo.style.display = 'none'
+        this.flowTo.innerHTML = ''
+        this.hiddenElements.forEach(node => node.style.display = '')
+        this.hiddenElements = []
     }
 
-    printReady() {
+    browserPrint() {
         window.print()
-        this.paginator.tearDown()
-        this.paginator = false
+    }
+
+    postPrint() {
         this.hideFlowTo()
     }
 
-    changeAllIds(node) {
+    cleanNode(node) {
+        if (node.classList.contains('ProseMirror-widget') || node.dataset.hidden==='true') {
+            node.parentElement.removeChild(node)
+            return
+        }
         if (node.id) {
             node.id = node.id + '-print'
         }
+        if (node.contentEditable === 'true') {
+            node.removeAttribute('contentEditable')
+        }
         if (node.children) {
-            for (let i=0;i<node.children.length;i++) {
-                this.changeAllIds(node.children[i])
-            }
+            Array.from(node.children).forEach(childNode => this.cleanNode(childNode))
         }
     }
 
@@ -51,61 +83,69 @@ export class ModToolsPrint {
         // This is a quick and dirty way of creating a cloned version of the node.
         // We only do this because it is faster and mathjax would be slow in rendering a second time.
 
-        let flowCopy = document.getElementById('flow').cloneNode(true)
+        const flowCopy = document.getElementById('flow').cloneNode(true)
 
-        this.changeAllIds(flowCopy)
+        this.cleanNode(flowCopy)
 
-        let footnoteBox = flowCopy.querySelector('#footnote-box-container-print')
+        const footnoteBox = flowCopy.querySelector('#footnote-box-container-print')
         footnoteBox.parentElement.removeChild(footnoteBox)
 
-        let footnotes = footnoteBox.querySelectorAll('.footnote-container')
-        let footnoteMarkers = flowCopy.querySelectorAll('.footnote-marker')
+        const footnotes = footnoteBox.querySelectorAll('.footnote-container')
+        const footnoteMarkers = flowCopy.querySelectorAll('.footnote-marker')
 
         footnoteMarkers.forEach((fnMarker, index) => {
             while (fnMarker.firstChild) {
                 fnMarker.removeChild(fnMarker.firstChild)
             }
+
             while (footnotes[index].firstChild) {
                 fnMarker.appendChild(footnotes[index].firstChild)
             }
+            fnMarker.innerHTML = 'footnote'
         })
-        let footnoteCitations = footnoteBox.querySelectorAll('.footnote-citation')
-        let footnoteCitationMarkers = flowCopy.querySelectorAll('.citation-footnote-marker')
+        const footnoteCitations = footnoteBox.querySelectorAll('.footnote-citation')
+        const footnoteCitationMarkers = flowCopy.querySelectorAll('.citation-footnote-marker')
 
         footnoteCitationMarkers.forEach((fnCitationMarker, index) => {
-            let fnCitation = footnoteCitations[index]
+            const fnCitation = footnoteCitations[index]
             fnCitation.classList.remove('footnote-citation')
             fnCitationMarker.appendChild(fnCitation)
             fnCitationMarker.classList.remove('citation-footnote-marker')
             fnCitationMarker.classList.add('footnote-marker')
         })
 
+        const styleSheets = [
+            '.footnote-marker {float: footnote;}',
+            document.getElementById('document-style') ? document.getElementById('document-style').innerHTML : '',
+            `@page {size: ${CSS_PAPER_SIZES[this.mod.editor.view.state.doc.firstChild.attrs.papersize]};}`
+        ]
+
         this.showFlowTo()
 
-        this.paginator = new PaginateForPrint({
-            'sectionStartSelector': 'none',
-            'chapterStartSelector': 'none',
-            'flowFromElement' : flowCopy,
-            'enableFrontmatter' : false,
-            'alwaysEven' : false,
-            'autoStart': false,
-            'pageWidth': 790,
-            'pageHeight': PAPER_HEIGHTS[this.mod.editor.view.state.doc.firstChild.attrs.papersize],
-            'outerMargin': 90,
-            'innerMargin': 90,
-            'contentsTopMargin': 80,
-            'headerTopMargin': 80,
-            'contentsBottomMargin': 80,
-            'pagenumberBottomMargin': 50,
-            'footnoteSelector': '.footnote-marker',
-            'lengthUnit': 'px',
-            'topfloatSelector': 'table,figure',
-            'flowToElement': this.flowTo,
-            'callback': () => {
-                this.printReady()
+        const docHTML = printTemplate({styleSheets, HTML: flowCopy.outerHTML}),
+            docBlob = new Blob([docHTML], {type : 'text/html'}),
+            docURL = URL.createObjectURL(docBlob),
+            Viewer = new viewer.Viewer(
+                {
+                    viewportElement: this.flowTo,
+                    userAgentRootURL: `${$StaticUrls.base$}vivliostyle-resources/`
+                }
+            )
+
+        Viewer.addListener('readystatechange', () => {
+            if (Viewer.readyState === 'complete') {
+                this.postProcess()
+                this.browserPrint()
+                this.postPrint()
             }
         })
-        this.paginator.initiate()
+
+        Viewer.loadDocument({url: docURL})
+
+    }
+
+    postProcess() {
+        this.flowTo.querySelectorAll('[data-vivliostyle-page-container]').forEach(node => node.style.display = 'block')
     }
 
 }

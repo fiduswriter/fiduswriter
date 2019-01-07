49e4fa33b81ef22b22767ed0487826141e4e7135
Rahul Arora <r.arora@alientt.com>
2019-01-07 16:21:25 +0530

    Added menu in table

--- document/static/js/modules/editor/state_plugins/tableMenu.js
+++ document/static/js/modules/editor/state_plugins/tableMenu.js
@@ -3,7 +3,7 @@ import {EditorState, Plugin, PluginKey, TextSelection} from "prosemirror-state"
 import {EditorView, Decoration, DecorationSet} from "prosemirror-view"
 import {history, redo, undo} from "prosemirror-history"
 import {keymap} from "prosemirror-keymap"
-
+import {tableMenuDialog} from '../dialogs'
 
 const key = new PluginKey('tableMenu')
 
@@ -25,100 +25,22 @@ const schema = new Schema({
 })
 
 
-const placeholderPlugin = function(nodeTitle) {
-    return new Plugin({
-        props: {
-            decorations: (state) => {
-                const doc = state.doc
-                if (
-                    doc.childCount === 1 &&
-                    doc.firstChild.isTextblock &&
-                    doc.firstChild.content.size === 0
-                ) {
-                    const placeHolder = document.createElement('span')
-                    placeHolder.classList.add('placeholder')
-                    // There is only one field, so we know the selection is there
-                    placeHolder.classList.add('selected')
-                    placeHolder.setAttribute(
-                        'data-placeholder',
-                        `${gettext('Add')} ${nodeTitle.toLowerCase()}...`
-                    )
-                    return DecorationSet.create(doc, [Decoration.widget(1, placeHolder)])
-                }
-            }
-        }
-    })
-}
-
-const pastePlugin = view => {
-
-    const submitTags = tags => {
-        const eState = view.state,
-            {decos} = key.getState(eState),
-            deco = decos.find()[0],
-            pos = deco.from,
-            nodes = tags.map(tag => eState.schema.nodes.tag.create({tag}))
-        view.dispatch(
-            view.state.tr.insert(pos, nodes)
-        )
-    }
-
-
-    return new Plugin({
-        props: {
-            transformPastedHTML: inHTML => {
-                const dom = document.createElement('div')
-                dom.innerHTML = inHTML
-                const tags = dom.innerText.split(/[,;]+/).filter(tag => tag.length)
-                if (tags.length) {
-                    const lastTag = tags.pop()
-                    submitTags(tags)
-                    return lastTag
-                } else {
-                    return inHTML
-                }
-            },
-            transformPastedText: inText => {
-                const tags = inText.split(/[,;]+/).filter(tag => tag.length)
-                if (tags.length) {
-                    const lastTag = tags.pop()
-                    submitTags(tags)
-                    return lastTag
-                } else {
-                    return inText
-                }
-            }
-        }
-    })
-}
-
-const submitTag = (tagState, dispatch, tagInputView, view, getPos) => {
-    const tag = tagState.doc.textContent
-    if (tag.length) {
-        const eState = view.state,
-            startPos = getPos(),
-            pos = startPos + view.state.doc.nodeAt(startPos).nodeSize - 1,
-            node = eState.schema.nodes.tag.create({tag})
-        view.dispatch(
-            view.state.tr.insert(pos, node)
-        )
-        tagInputView.dispatch(
-            tagState.tr.delete(1, tagState.doc.nodeSize - 3)
-        )
-    }
-    return true
-}
-
-
 export class TableView {
-    constructor(node, view, getPos) {
+    constructor(node, view, getPos,options) {
         this.node = node
         this.view = view
         this.getPos = getPos
+        this.options = this.options
         this.dom = document.createElement("div")
         var a = document.createElement("button")
         a.className = "testing"
         this.dom.appendChild(a)
+        this.dom.lastElementChild.addEventListener('click', event => {
+            event.preventDefault()
+            const dialog = new tableMenuDialog(node, view, options)
+            dialog.init()
+            
+        })
         this.contentDOM = this.dom.appendChild(document.createElement("table"))
         this.dom.appendChild(this.contentDOM)
     }
@@ -141,7 +63,7 @@ export const tableMenuPlugin = function(options) {
             init(config, state) {
                 if (options.editor.docInfo.access_rights === 'write') {
                     this.spec.props.nodeViews['table'] =
-                        (node, view, getPos) => new TableView(node, view, getPos)
+                        (node, view, getPos) => new TableView(node, view, getPos,options)
                 }
 
                 return {}

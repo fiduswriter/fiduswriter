dfcef4f709d96ee50badbced6919c50938dff7e6
Rahul Arora <r.arora@alientt.com>
2019-02-07 17:04:46 +0530

    Added a filter option in navigator

--- document/static/js/modules/editor/navigator/index.js
+++ document/static/js/modules/editor/navigator/index.js
@@ -1,10 +1,13 @@
 import {escapeText ,findTarget} from "../../common"
+import {navigatorFilterModel} from "./model"
 export class ModNavigator {
     constructor(editor) {
         editor.mod.navigator = this
         this.editor = editor
         this.navigatorEl = document.querySelector('#navigator')
         this.listeners = {}
+        this.navigatorFilters = navigatorFilterModel().content
+        this.defaultFilters = ['heading1','heading2']
         this.render()
         this.bindEvents()
     }
@@ -28,6 +31,29 @@ export class ModNavigator {
                     event.stopImmediatePropagation()
                     document.getElementById(el.target.getAttribute('href').slice(1)).scrollIntoView({behavior:"smooth"})
                     break
+                case findTarget(event, '#navigator-filter-icon', el):
+                    if (document.getElementById("navigator-filter").classList.contains('hide')){
+                        this.showFilters()
+                    } else {
+                        this.hideFilters()
+                    }
+                    break
+                case findTarget(event, '#navigator-filter-back', el):
+                    this.defaultFilters = []
+                    document.querySelectorAll('#navigator-filter input').forEach(
+                        item => {
+                            if (item.checked){
+                                this.defaultFilters.push(item.id)
+                            }
+                        }
+                    )
+                    document.querySelector('#navigator-list').innerHTML = this.populateNavigator() || ""
+                    this.hideFilters()
+                    break
+                case findTarget(event, 'input', el):
+                    break
+                case findTarget(event, 'label', el):
+                    break
                 default:
                     this.closeNavigator()
                     break
@@ -37,17 +63,36 @@ export class ModNavigator {
     openNavigator(){
         document.getElementById('navigator-button').firstElementChild.firstElementChild.classList.add('rotate')
         document.getElementById('navigator').style.left = "0px"
+
+        document.getElementById("navigator-filter").classList.add('hide')
+        document.getElementById("navigator-list").classList.remove('hide')
+        document.getElementById('navigator-filter-back').classList.add('hide')
+        document.getElementById('navigator-filter-icon').classList.remove('hide')
     }
     closeNavigator(){
         document.getElementById('navigator-button').firstElementChild.firstElementChild.classList.remove('rotate')
         document.getElementById('navigator').style.left = "-265px"
+    }
+    showFilters(){
+        document.getElementById("navigator-filter").classList.remove('hide')
+        document.getElementById('navigator-filter-back').classList.remove('hide')
+        document.getElementById("navigator-list").classList.add('hide')
+        document.getElementById('navigator-filter-icon').classList.add('hide')
+        //populating the filter list
+        document.getElementById('navigator-filter').innerHTML = this.populateNavFilter()
+    }
+    hideFilters(){
+        document.getElementById("navigator-filter").classList.add('hide')
+        document.getElementById('navigator-filter-back').classList.add('hide')
+        document.getElementById("navigator-list").classList.remove('hide')
+        document.getElementById('navigator-filter-icon').classList.remove('hide') 
     }
     populateNavigator(){
         const items = []
         this.editor.view.state.doc.descendants((node) => {
             if (node.attrs && node.attrs.hidden) {
                 return false
-            } else if (node.type.groups.includes('heading')) {
+            } else if (this.defaultFilters.includes(node.type.name)) {
                 items.push({id: node.attrs.id, textContent: node.textContent, type: node.type})
             }
         })
@@ -55,6 +100,26 @@ export class ModNavigator {
             return this.navigatorHTML(items)
         } else {
             return false
+        }
+    }
+    populateNavFilter(){
+        return `
+        ${
+            this.navigatorFilters.map(
+                item => {
+                    const level = item.level
+                    return `<span><input type="checkbox" class="form-checkbox" id="heading${level}" ${this.inDefault(level)} />
+                                <label for="heading${level}">${item.title}</label>
+                            </span><br/><br/>`
+                }
+            ).join('')
+        }`
+    }
+    inDefault(level){
+        if (this.defaultFilters.includes('heading'+level)){
+            return 'checked'
+        } else {
+            return ''
         }
     }
     navigatorHTML(items) {
@@ -71,8 +136,14 @@ export class ModNavigator {
     getNavigatorTemplate(){
         return `
                 <div id="navigator-content">
-                    <h1 class="header">${gettext('Document Navigator')}</h1>
+                    <div class="header-container">
+                        <span id="navigator-filter-back" class="hide"><i class="fas fa-arrow-left"></i></span>
+                        <h1 class="header">${gettext('Document Navigator')}</h1>
+                        <span id="navigator-filter-icon"><i class="fas fa-filter"></i></span>
+                    </div>
                     <div id="navigator-list"></div>
+                    <div id="navigator-filter" class="hide">
+                    </div>
                 </div>
                 <div id="navigator-button">
                     <span class="navigator-arrow-icon"><i class="fas fa-angle-right"></i></span>
---
+++ document/static/js/modules/editor/navigator/model.js
@@ -0,0 +1,28 @@
+export const navigatorFilterModel = ()=> ({
+    content: [
+        {
+            title: 'Heading 1',
+            level: 1
+        },
+        {
+            title: 'Heading 2',
+            level: 2
+        },
+        {
+            title: 'Heading 3',
+            level: 3
+        },
+        {
+            title: 'Heading 4',
+            level: 4
+        },
+        {
+            title: 'Heading 5',
+            level: 5
+        },
+        {
+            title: 'Heading 6',
+            level: 6
+        }
+    ]
+})

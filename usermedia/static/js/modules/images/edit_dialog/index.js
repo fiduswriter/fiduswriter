d1e7b8ae5305aa74dd196e647c520e9ab840e851
Rahul Arora <r.arora@alientt.com>
2019-02-18 11:39:40 +0530

    Rotating Image by drawing it on canvas

--- usermedia/static/js/modules/images/edit_dialog/index.js
+++ usermedia/static/js/modules/images/edit_dialog/index.js
@@ -54,15 +54,44 @@ export class ImageEditDialog {
 
         selectButton.addEventListener('click', () => mediaInput.click())
 
-        mediaInput.addEventListener('change', function() {
+        mediaInput.addEventListener('change', () =>{
             const file = mediaInput.files[0],
                 fr = new window.FileReader()
-
             fr.onload = () => {
-                mediaPreviewer.innerHTML = '<img src="' + fr.result + '" />'
+                this.rotateBase64Image(fr.result,this.callback)
+               // mediaPreviewer.innerHTML = '<img src="' + t + '" />'
             }
             fr.readAsDataURL(file)
+            console.log(file)
         })
+    }
+
+    rotateBase64Image(base64data, callback) {
+        const canvas = document.createElement("canvas")
+        const ctx = canvas.getContext("2d")
+        const image = new Image()
+        image.src = base64data
+        image.onload = ()=> {
+            canvas.height = image.width
+            canvas.width = image.height
+
+            ctx.rotate(90 * Math.PI / 180)
+            ctx.translate(0, -canvas.width)
+            ctx.drawImage(image, 0, 0);
+            callback(canvas.toDataURL())
+        }
+    }
+    callback(base64data){
+        const mediaPreviewer = document.querySelector('#editimage .figure-preview > div')
+        mediaPreviewer.innerHTML = '<img src="' + base64data + '" />'
+
+        var arr = base64data.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        const f =  new File([u8arr], "test.jpeg", {type:mime});
+        
     }
 
     displayCreateImageError(errors) {
@@ -85,6 +114,18 @@ export class ImageEditDialog {
     }
 
     saveImage() {
+        
+        const mediaPreviewer = document.querySelector('#editimage .figure-preview > div > img')
+        const base64data = mediaPreviewer.src;
+
+        var arr = base64data.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        console.log(arr[0],mime)
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        const f =  new File([u8arr], "test.png", {type:mime});
+
 
         const imageData = {
             title: document.querySelector('#editimage .fw-media-title').value,
@@ -96,14 +137,13 @@ export class ImageEditDialog {
         if (this.imageId) {
             imageData.id = this.imageId
         } else {
-            imageData.image = document.querySelector('#editimage .fw-media-file-input').files[0]
+            imageData.image = f//document.querySelector('#editimage .fw-media-file-input').files[0]
         }
-
+        console.log(imageData.image)
         // Remove old warning messages
         document.querySelectorAll('#editimage .warning').forEach(
             el => el.parentElement.removeChild(el)
         )
-
         return new Promise(resolve => {
             this.imageDB.saveImage(imageData).then(
                 imageId => {

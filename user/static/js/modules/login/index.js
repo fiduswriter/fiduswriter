cb691dd7eb637233af0197fcc438d0a39917d6c7
Johannes Wilm <mail@johanneswilm.org>
2019-03-30 01:29:58 +0100

    better login page plugin support

--- user/static/js/modules/login/index.js
+++ user/static/js/modules/login/index.js
@@ -1,25 +1,46 @@
 import {whenReady} from "../common"
+import * as plugins from "../../plugins/login"
 
-const loginPage = function() {
-    const socialButtons = document.querySelectorAll('.fw-button.fw-socialaccount')
-    let btnWidth = 1
 
-    socialButtons.forEach(
-        button => {
-            const theWidth = button.clientWidth
-            if (btnWidth < theWidth) {
-                btnWidth = theWidth
+export class LoginPage {
+    constructor({staticUrl}) {
+        this.staticUrl = staticUrl
+    }
+
+    init() {
+        this.activateFidusPlugins()
+        whenReady().then(
+            () => this.bind()
+        )
+    }
+
+    activateFidusPlugins() {
+        // Add plugins.
+        this.plugins = {}
+
+        Object.keys(plugins).forEach(plugin => {
+            if (typeof plugins[plugin] === 'function') {
+                this.plugins[plugin] = new plugins[plugin]({page: this, staticUrl: this.staticUrl})
+                this.plugins[plugin].init()
             }
-        }
-    )
-    btnWidth += 15
-    socialButtons.forEach(
-        button => button.style.width = `${btnWidth}px`
-    )
-}
+        })
+    }
 
-export const bind = function() {
-    whenReady().then(() => {
-        loginPage()
-    })
+    bind() {
+        const socialButtons = document.querySelectorAll('.fw-button.fw-socialaccount')
+        let btnWidth = 1
+
+        socialButtons.forEach(
+            button => {
+                const theWidth = button.clientWidth
+                if (btnWidth < theWidth) {
+                    btnWidth = theWidth
+                }
+            }
+        )
+        btnWidth += 15
+        socialButtons.forEach(
+            button => button.style.width = `${btnWidth}px`
+        )
+    }
 }

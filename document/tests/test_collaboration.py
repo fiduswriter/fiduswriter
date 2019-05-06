5a1d139b3abc114fa396b1ceb11373f1911492f5
Johannes Wilm <mail@johanneswilm.org>
2019-05-06 23:48:39 +0200

    remove debug

--- document/tests/test_collaboration.py
+++ document/tests/test_collaboration.py
@@ -911,10 +911,7 @@ class OneUserTwoBrowsersTests(LiveTornadoTestCase, EditorHelper):
 
         # Wait for the two editors to be synched
         self.wait_for_doc_sync(self.driver, self.driver2)
-        print(self.get_mathequation(self.driver2))
-        print(self.get_mathequation(self.driver2))
-        print(self.get_mathequation(self.driver2))
-        print(len(self.get_mathequation(self.driver2)))
+
         self.assertEqual(
             len(self.get_mathequation(self.driver)),
             len(self.get_mathequation(self.driver2))

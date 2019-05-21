20a3e013217d4f9bb163fa684c5e436ac1c262ea
Johannes Wilm <mail@johanneswilm.org>
2019-05-21 14:09:03 +0200

    remove TeXZilla

--- document/static/js/modules/exporter/odt/math.js
+++ document/static/js/modules/exporter/odt/math.js
@@ -1,5 +1,3 @@
-import TeXZilla from "texzilla"
-
 export class OdtExporterMath {
     constructor(exporter) {
         this.exporter = exporter
@@ -42,17 +40,6 @@ export class OdtExporterMath {
     }
 
     addMath(latex) {
-        // const mathml = this.domParser.parseFromString(
-        //     `<math xmlns="http://www.w3.org/1998/Math/MathML">${
-        //         this.mathLive.latexToMathML(latex)//.replace(/\&InvisibleTimes;/g, '&#8290;')
-        //     }</math>`,
-        //     "application/xml"
-        // ).documentElement
-        // console.log(this.mathLive.latexToMathML(latex))
-        // const mathml2 = TeXZilla.toMathML(latex)
-        // console.log({mathml: mathml.outerHTML, mathml2: mathml2.outerHTML})
-        const objectNumber = this.objectCounter++
-        //this.exporter.xml.addXmlFile(`Object ${objectNumber}/content.xml`, mathml)
         this.exporter.xml.addExtraFile(
             `Object ${objectNumber}/content.xml`,
             `<math xmlns="http://www.w3.org/1998/Math/MathML">${

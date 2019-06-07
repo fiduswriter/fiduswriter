0af770fa09931f6c9d524099fe9eefdb90219714
Anand <a.sekar@alientt.com>
2019-06-07 17:11:01 +0530

    Removed trailing white spaces and semi colons

--- document/static/js/modules/document_template/designer.js
+++ document/static/js/modules/document_template/designer.js
@@ -251,7 +251,7 @@ export class DocumentTemplateDesigner {
                                 break
                         }
                         if(el.classList.contains("error-element")){
-                            el.classList.remove("error-element");
+                            el.classList.remove("error-element")
                         }
                         if (!id.length) {
                             valid = false
@@ -262,7 +262,7 @@ export class DocumentTemplateDesigner {
                         if (/\s/.test(id)) {
                             valid = false
                             this.errors.no_spaces = gettext('IDs cannot contain spaces.')
-                            el.classList.add("error-element");
+                            el.classList.add("error-element")
                             el.scrollIntoView({block:"center" , behavior :"smooth"})
                         }
                         if (ids.includes(id)) {
@@ -271,7 +271,7 @@ export class DocumentTemplateDesigner {
                                 el => {
                                     const id_duplicate = el.querySelector('input.id').value
                                     if(id_duplicate == id ){
-                                        el.classList.add("error-element");  
+                                        el.classList.add("error-element")
                                     }
                                 })
                             el.scrollIntoView({block:"center" , behavior :"smooth"})

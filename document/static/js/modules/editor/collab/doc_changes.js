52300106b6c355810aa46eae1e6f8ca652866815
Johannes Wilm <mail@johanneswilm.org>
2019-06-20 00:25:46 +0200

    store minimal doc json

--- document/static/js/modules/editor/collab/doc_changes.js
+++ document/static/js/modules/editor/collab/doc_changes.js
@@ -1,7 +1,7 @@
 import {
     compare
 } from "fast-json-patch"
-
+import deepEqual from "fast-deep-equal"
 import {
     sendableSteps,
     receiveTransaction
@@ -15,17 +15,51 @@ import {
     updateCollaboratorSelection
 } from "../state_plugins"
 
-function noEmptyTrack(node) {
-    if (node.attrs && node.attrs.track && !node.attrs.track.length) {
-        delete node.attrs.track
-        if (!Object.keys(node.attrs).length) {
-            delete node.attrs
+function toMiniJSON(node) {
+    // Similar to the ProseMirror internal toJSON function,
+    // but leaving out attributes that have default values and dealing with
+    // attributes that are objects.
+    // Adapted from https://github.com/ProseMirror/prosemirror-model/blob/6d970507cd0da48653d3b72f2731a71a144a364b/src/node.js#L340-L351
+    const obj = {type: node.type.name}
+    for (const attr in node.attrs) {
+        if (!deepEqual(node.type.attrs[attr].default, node.attrs[attr])) {
+            if (!obj.attrs) {
+                obj.attrs = {}
+            }
+            obj.attrs[attr] = ["string", "number", "boolean"].includes(typeof node.attrs[attr]) ?
+                node.attrs[attr] :
+                JSON.parse(JSON.stringify(node.attrs[attr]))
         }
     }
-    if (node.content) {
-        node.content.forEach(child => noEmptyTrack(child))
+    if (node.content.size) {
+        obj.content = node.content.content.map(childNode => toMiniJSON(childNode))
     }
-    return node
+
+    if (node.marks.length) {
+        obj.marks = node.marks.map(mark => toMiniMarkJSON(mark))
+    }
+
+    if (node.text) {
+        obj.text = node.text
+    }
+
+    return obj
+}
+
+function toMiniMarkJSON(mark) {
+    // Adapted from https://github.com/ProseMirror/prosemirror-model/blob/6d970507cd0da48653d3b72f2731a71a144a364b/src/mark.js#L76-L83
+    const obj = {type: mark.type.name}
+    for (const attr in mark.attrs) {
+        if (!deepEqual(mark.type.attrs[attr].default, mark.attrs[attr])) {
+            if (!obj.attrs) {
+                obj.attrs = {}
+            }
+            obj.attrs[attr] = ["string", "number", "boolean"].includes(typeof mark.attrs[attr]) ?
+                mark.attrs[attr] :
+                JSON.parse(JSON.stringify(mark.attrs[attr]))
+        }
+    }
+    return obj
 }
 
 export class ModCollabDocChanges {
@@ -140,9 +174,9 @@ export class ModCollabDocChanges {
                     // server.
                     unconfirmedDiff['jd'] = compare(
                         this.mod.editor.docInfo.confirmedJson,
-                        noEmptyTrack(JSON.parse(JSON.stringify(
-                            this.mod.editor.view.state.doc.firstChild.toJSON()
-                        )))
+                        toMiniJSON(
+                            this.mod.editor.view.state.doc.firstChild
+                        )
                     )
                     // In case the title changed, we also add a title field to
                     // update the title field instantly - important for the
@@ -301,9 +335,7 @@ export class ModCollabDocChanges {
         this.mod.editor.docInfo.confirmedDoc = docNumber === tr.docs.length ?
             tr.doc :
             tr.docs[docNumber]
-        this.mod.editor.docInfo.confirmedJson = noEmptyTrack(JSON.parse(JSON.stringify(
-            this.mod.editor.docInfo.confirmedDoc.firstChild.toJSON()
-        )))
+        this.mod.editor.docInfo.confirmedJson = toMiniJSON(this.mod.editor.docInfo.confirmedDoc.firstChild)
     }
 
     confirmDiff(request_id) {
@@ -325,9 +357,9 @@ export class ModCollabDocChanges {
             )
             this.mod.editor.view.dispatch(tr)
             this.mod.editor.docInfo.confirmedDoc = unconfirmedDiffs["doc"]
-            this.mod.editor.docInfo.confirmedJson = noEmptyTrack(JSON.parse(JSON.stringify(
-                this.mod.editor.docInfo.confirmedDoc.firstChild.toJSON()
-            )))
+            this.mod.editor.docInfo.confirmedJson = toMiniJSON(
+                this.mod.editor.docInfo.confirmedDoc.firstChild
+            )
         }
 
         const sentFnSteps = unconfirmedDiffs["fs"] // footnote steps

7ab2fcdd064797d04575035f27cf3a5048246699
Johannes Wilm <mail@johanneswilm.org>
2019-08-19 17:37:09 +0200

    lint

--- fiduswriter/document/static/js/modules/document_template/designer.js
+++ fiduswriter/document/static/js/modules/document_template/designer.js
@@ -9,7 +9,6 @@ import {menuBar} from "prosemirror-menu"
 import {buildKeymap, buildInputRules} from "prosemirror-example-setup"
 import {tableEditing} from "prosemirror-tables"
 
-import {randomHeadingId} from "../schema/common"
 import {TagsView, ContributorsView} from "../editor/state_plugins"
 import {
     documentDesignerTemplate,
@@ -30,77 +29,12 @@ import {
     templateHash
 } from "./schema"
 
-// from https://codeburst.io/throttling-and-debouncing-in-javascript-646d076d0a44
-function debounced(delay, fn) {
-    let timerId
-    return function(...args) {
-        if (timerId) {
-            clearTimeout(timerId)
-        }
-        timerId = setTimeout(() => {
-            fn(...args)
-            timerId = null
-        }, delay)
-    }
-}
+import {
+    debounced,
+    noTrack,
+    addHeadingIds
+} from "./tools"
 
-function noTrack(node) {
-    if (node.attrs && node.attrs.track) {
-        delete node.attrs.track
-        if (!Object.keys(node.attrs).length) {
-            delete node.attrs
-        }
-    }
-    if (node.content) {
-        node.content.forEach(child => noTrack(child))
-    }
-    return node
-}
-
-function addHeadingIds(oldState, newState, editors) {
-    const newHeadings = [],
-        usedHeadingIds = []
-
-    editors.forEach(([_el, view]) => {
-        if (view.state === oldState) {
-            return
-        }
-        view.state.doc.descendants(node => {
-            if (node.type.groups.includes('heading')) {
-                usedHeadingIds.push(node.attrs.id)
-            }
-        })
-    })
-    newState.doc.descendants((node, pos) => {
-        if (node.type.groups.includes('heading')) {
-            if (node.attrs.id === false || usedHeadingIds.includes(node.attrs.id)) {
-                newHeadings.push({pos, node})
-            } else {
-                usedHeadingIds.push(node.attrs.id)
-            }
-
-        }
-    })
-    if (!newHeadings.length) {
-        return null
-    }
-    const newTr = newState.tr
-    newHeadings.forEach(
-        newHeading => {
-            let id
-            while (!id || usedHeadingIds.includes(id)) {
-                id = randomHeadingId()
-            }
-            usedHeadingIds.push(id)
-            newTr.setNodeMarkup(
-                newHeading.pos,
-                null,
-                Object.assign({}, newHeading.node.attrs, {id})
-            )
-        }
-    )
-    return newTr
-}
 
 export class DocumentTemplateDesigner {
     constructor({staticUrl}, id, title, value, citationStyles, documentStyles, exportTemplates, dom) {

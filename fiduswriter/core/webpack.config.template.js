c37371afaa1fafba3d08be4278f1dc10f1db144e
Johannes Wilm <mail@johanneswilm.org>
2019-08-30 18:15:29 +0200

    do not cache csljson files

--- fiduswriter/core/webpack.config.template.js
+++ fiduswriter/core/webpack.config.template.js
@@ -57,7 +57,11 @@ module.exports = { // eslint-disable-line no-undef
                 'admin_console.js',
                 'maintenance.js',
                 'test_caret.js',
-                'document_template_admin.js'
+                'document_template_admin.js',
+                '**/.*',
+                '**/*.map',
+                '**/*.gz',
+                '**/*.csljson'
             ]
         })
     ],

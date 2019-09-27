93ff88781a9d35b447702b9088e520f44327d309
Johannes Wilm <mail@johanneswilm.org>
2019-09-27 16:13:04 +0200

    fix doc: allow template without content

--- fiduswriter/document/static/js/modules/document_template/fix_doc.js
+++ fiduswriter/document/static/js/modules/document_template/fix_doc.js
@@ -92,117 +92,120 @@ export function adjustDocToTemplate(miniDoc, miniTemplate, documentStyles, schem
 
     let movedParts = []
 
-    template.content.slice(1).forEach(part => {
-        let oldNode = oldContent.find(
-            oldContentNode =>
-                oldContentNode.type === part.type &&
-                oldContentNode.attrs.id === part.attrs.id
-        )
-        if (oldNode) {
-            while (oldNode !== oldContent[0]) {
-                const firstOldContent = oldContent.shift(),
-                    inTemplate = !!template.content.find(
-                        part =>
-                            part.type === firstOldContent.type &&
-                            part.attrs.id === firstOldContent.attrs.id
-                    )
-                if (inTemplate) {
-                    movedParts.push(firstOldContent)
-                } else if (
-                    firstOldContent.content &&
-                    !firstOldContent.attrs.hidden &&
-                    firstOldContent.attrs.locking !== 'fixed' &&
-                    !(
-                        // table with just first row, which is fixed.
-                        firstOldContent.attrs.locking === 'header' &&
-                        firstOldContent.content.length === 1
-                    ) &&
-                    !(
-                        // heading/richtext with just the default contents
-                        firstOldContent.attrs.elements &&
-                        firstOldContent.content.length === 1 &&
-                        firstOldContent.content[0].type === firstOldContent.attrs.elements[0] &&
-                        !firstOldContent.content[0].content
-                    )
-                ) {
-                    firstOldContent.attrs.deleted = true
-                    doc.content.push(firstOldContent)
-                }
-            }
-            oldContent.shift()
-        } else {
-            oldNode = movedParts.find(
+    if (template.content) {
+        template.content.slice(1).forEach(part => {
+            let oldNode = oldContent.find(
                 oldContentNode =>
                     oldContentNode.type === part.type &&
                     oldContentNode.attrs.id === part.attrs.id
             )
             if (oldNode) {
-                movedParts = movedParts.filter(oldContentNode => oldContentNode !== oldNode)
-            }
-        }
-        if (oldNode) {
-            const newNode = Object.assign(
-                    {},
-                    oldNode,
-                    {
-                        attrs: {}
+                while (oldNode !== oldContent[0]) {
+                    const firstOldContent = oldContent.shift(),
+                        inTemplate = !!template.content.find(
+                            part =>
+                                part.type === firstOldContent.type &&
+                                part.attrs.id === firstOldContent.attrs.id
+                        )
+                    if (inTemplate) {
+                        movedParts.push(firstOldContent)
+                    } else if (
+                        firstOldContent.content &&
+                        !firstOldContent.attrs.hidden &&
+                        firstOldContent.attrs.locking !== 'fixed' &&
+                        !(
+                            // table with just first row, which is fixed.
+                            firstOldContent.attrs.locking === 'header' &&
+                            firstOldContent.content.length === 1
+                        ) &&
+                        !(
+                            // heading/richtext with just the default contents
+                            firstOldContent.attrs.elements &&
+                            firstOldContent.content.length === 1 &&
+                            firstOldContent.content[0].type === firstOldContent.attrs.elements[0] &&
+                            !firstOldContent.content[0].content
+                        )
+                    ) {
+                        firstOldContent.attrs.deleted = true
+                        doc.content.push(firstOldContent)
                     }
+                }
+                oldContent.shift()
+            } else {
+                oldNode = movedParts.find(
+                    oldContentNode =>
+                        oldContentNode.type === part.type &&
+                        oldContentNode.attrs.id === part.attrs.id
                 )
-            Object.entries(part.attrs).forEach(([key, value]) => {
-                newNode.attrs[key] = value
-            })
-            if (newNode.attrs.optional) {
-                newNode.attrs.hidden = oldNode.attrs.hidden
-            }
-            if (
-                (newNode.attrs.initial || oldNode.attrs.initial) &&
-                (
-                    oldNode.attrs.locking === 'fixed' ||
-
-                    hash(oldNode.attrs.initial || {}) === hash(oldNode.content || {})
-                )
-            ) {
-                if (newNode.attrs.initial) {
-                    newNode.content = newNode.attrs.initial
-                } else {
-                    delete newNode.content
+                if (oldNode) {
+                    movedParts = movedParts.filter(oldContentNode => oldContentNode !== oldNode)
                 }
             }
+            if (oldNode) {
+                const newNode = Object.assign(
+                        {},
+                        oldNode,
+                        {
+                            attrs: {}
+                        }
+                    )
+                Object.entries(part.attrs).forEach(([key, value]) => {
+                    newNode.attrs[key] = value
+                })
+                if (newNode.attrs.optional) {
+                    newNode.attrs.hidden = oldNode.attrs.hidden
+                }
+                if (
+                    (newNode.attrs.initial || oldNode.attrs.initial) &&
+                    (
+                        oldNode.attrs.locking === 'fixed' ||
 
-            if (oldNode.attrs.elements) { // parts that define elements also define marks.
-                const removedElements = oldNode.attrs.elements.filter(
-                    element => !newNode.attrs.elements.includes(element)
-                )
-                const removedMarks = oldNode.attrs.marks.filter(
-                    mark => !newNode.attrs.marks.includes(mark)
-                )
-                if (removedElements.length || removedMarks.length) {
-                    cleanNode(newNode, removedElements, removedMarks)
-                    if (!newNode.content && ['richtext_part', 'heading_part'].includes(part.type)) {
-                        newNode.content = [{type: part.attrs.elements[0]}]
-                    } else if (!newNode.content && part.type === 'table_part') {
-                        newNode.content = [
-                            {type: 'table', content: [
-                                {type: 'table_row', content: [
-                                    {type: 'table_cell', content: [{type: 'paragraph'}]}
+                        hash(oldNode.attrs.initial || {}) === hash(oldNode.content || {})
+                    )
+                ) {
+                    if (newNode.attrs.initial) {
+                        newNode.content = newNode.attrs.initial
+                    } else {
+                        delete newNode.content
+                    }
+                }
+
+                if (oldNode.attrs.elements) { // parts that define elements also define marks.
+                    const removedElements = oldNode.attrs.elements.filter(
+                        element => !newNode.attrs.elements.includes(element)
+                    )
+                    const removedMarks = oldNode.attrs.marks.filter(
+                        mark => !newNode.attrs.marks.includes(mark)
+                    )
+                    if (removedElements.length || removedMarks.length) {
+                        cleanNode(newNode, removedElements, removedMarks)
+                        if (!newNode.content && ['richtext_part', 'heading_part'].includes(part.type)) {
+                            newNode.content = [{type: part.attrs.elements[0]}]
+                        } else if (!newNode.content && part.type === 'table_part') {
+                            newNode.content = [
+                                {type: 'table', content: [
+                                    {type: 'table_row', content: [
+                                        {type: 'table_cell', content: [{type: 'paragraph'}]}
+                                    ]}
                                 ]}
-                            ]}
-                        ]
+                            ]
+                        }
                     }
                 }
-            }
-            doc.content.push(newNode)
+                doc.content.push(newNode)
 
-        } else {
-            // The node is new and didn't exist in the old document.
-            doc.content.push(JSON.parse(JSON.stringify(part)))
-        }
-    })
+            } else {
+                // The node is new and didn't exist in the old document.
+                doc.content.push(JSON.parse(JSON.stringify(part)))
+            }
+        })
+    }
 
     // move remaining oldContent items that were not in template.
     while (oldContent.length) {
         const newNode = oldContent.shift()
-        if (newNode.attrs.hasOwnProperty('deleted')) {
+        const attrs = Object.assign({}, newNode.attrs)
+        if (attrs.hasOwnProperty('deleted')) {
             newNode.attrs.deleted = true
             doc.content.push(newNode)
         }

971a5bbdbb3f2b28cfd5405326c0bbe2ac15699a
Johannes Wilm <mail@johanneswilm.org>
2020-01-11 02:53:26 +0100

    lint

--- fiduswriter/base/management/commands/setup.py
+++ fiduswriter/base/management/commands/setup.py
@@ -1,5 +1,5 @@
 import os
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.conf import settings
 from django.contrib.flatpages.models import FlatPage

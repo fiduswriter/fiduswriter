33d3d3d23e0bdc5c2e9d5253254d664644446e0d
Johannes Wilm <mail@johanneswilm.org>
2020-02-09 23:15:36 +0100

    simplify style/views.py

--- fiduswriter/style/views.py
+++ fiduswriter/style/views.py
@@ -16,20 +16,13 @@ from document.models import DocumentTemplate
 def delete_document_style(request):
     response = {}
     id = int(request.POST['id'])
-    if request.user.is_staff:
-        document_style = DocumentStyle.objects.filter(id=id).first()
-    else:
-        document_style = DocumentStyle.objects.filter(
-            id=id,
-            document_template__user=request.user
-        ).first()
-    if not document_style:
-        return JsonResponse(
-            response,
-            status=405
-        )
+    style_selector = {'id': id}
+    if not request.user.is_staff:
+        style_selector['document_template__user'] = request.user
+    document_style = DocumentStyle.objects.get(**style_selector)
     if document_style.document_template.documentstyle_set.all().count() < 2:
-        # We do not the style if there is only one left for this template
+        # We do not delete the style if there is only
+        # one left for this template
         response['errors'] = {
             'template': _('The template needs at least 1 document style')
         }
@@ -51,34 +44,23 @@ def delete_document_style(request):
 def save_document_style(request):
     response = {}
     template_id = int(request.POST['template_id'])
-    if request.user.is_staff:
-        template = DocumentTemplate.objects.filter(id=template_id).first()
-    else:
-        template = DocumentTemplate.objects.filter(
-            id=template_id,
-            user=request.user
-        ).first()
-    if not template:
-        return JsonResponse(
-            response,
-            status=405
-        )
+    template_selector = {'id': template_id}
+    if not request.user.is_staff:
+        template_selector['user'] = request.user
+    template = DocumentTemplate.objects.get(
+        **template_selector
+    )
     id = int(request.POST['id'])
     if id > 0:
-        document_style = DocumentStyle.objects.filter(
+        document_style = DocumentStyle.objects.get(
             id=id,
             document_template=template
-        ).first()
+        )
         status = 200
     else:
         document_style = DocumentStyle()
         document_style.document_template = template
         status = 201
-    if not document_style:
-        return JsonResponse(
-            response,
-            status=405
-        )
     document_style.title = request.POST['title']
     document_style.slug = request.POST['slug']
     document_style.contents = request.POST['contents']
@@ -123,18 +105,12 @@ def save_document_style(request):
 def delete_export_template(request):
     response = {}
     id = int(request.POST['id'])
-    if request.user.is_staff:
-        export_template = ExportTemplate.objects.filter(id=id).first()
-    else:
-        export_template = ExportTemplate.objects.filter(
-            id=id,
-            document_template__user=request.user
-        ).first()
-    if not export_template:
-        return JsonResponse(
-            response,
-            status=405
-        )
+    template_selector = {'id': id}
+    if not request.user.is_staff:
+        template_selector['document_template__user'] = request.user
+    export_template = ExportTemplate.objects.get(
+        **template_selector
+    )
     export_template.delete()
     status = 200
     return JsonResponse(
@@ -149,34 +125,23 @@ def delete_export_template(request):
 def save_export_template(request):
     response = {}
     template_id = int(request.POST['template_id'])
-    if request.user.is_staff:
-        template = DocumentTemplate.objects.filter(id=template_id).first()
-    else:
-        template = DocumentTemplate.objects.filter(
-            id=template_id,
-            user=request.user
-        ).first()
-    if not template:
-        return JsonResponse(
-            response,
-            status=405
-        )
+    template_selector = {'id': template_id}
+    if not request.user.is_staff:
+        template_selector['user'] = request.user
+    template = DocumentTemplate.objects.get(
+        **template_selector
+    )
     id = int(request.POST['id'])
     if id > 0:
-        export_template = ExportTemplate.objects.filter(
+        export_template = ExportTemplate.objects.get(
             id=id,
             document_template=template
-        ).first()
+        )
         status = 200
     else:
         export_template = ExportTemplate()
         export_template.document_template = template
         status = 201
-    if not export_template:
-        return JsonResponse(
-            response,
-            status=405
-        )
     export_template.template_file = request.FILES['added_file']
     export_template.file_type = request.POST['added_file_type']
     try:

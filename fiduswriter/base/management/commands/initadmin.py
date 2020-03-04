d2490ba96c858e70269f935a9b2d743a007208ee
Johannes Wilm <mail@johanneswilm.org>
2020-03-05 00:21:05 +0100

    lint

--- fiduswriter/base/management/commands/initadmin.py
+++ fiduswriter/base/management/commands/initadmin.py
@@ -1,11 +1,14 @@
-# code adapted by github.com/jobdiogenes from https://github.com/dkarchmer/aws-eb-docker-django/blob/master/authentication/manage
+# code adapted by github.com/jobdiogenes from
+# https://github.com/dkarchmer/aws-eb-docker-django/blob/master
+# /authentication/manage
 # used to help automation install like in docker.
 # Create admins accounts if no users exists.
-# Password 'admin' is used unless defined by ADMIN_PASSWORD 
+# Password 'admin' is used unless defined by ADMIN_PASSWORD
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from os import getenv
+
 
 class Command(BaseCommand):
 
@@ -14,11 +17,20 @@ class Command(BaseCommand):
             for user in settings.ADMINS:
                 username = user[0].replace(' ', '')
                 email = user[1]
-                password = getenv('ADMIN_PASSWORD') if getenv('ADMIN_PASSWORD')!='' else 'admin'
+                if getenv('ADMIN_PASSWORD') != '':
+                    password = getenv('ADMIN_PASSWORD')
+                else:
+                    password = 'admin'
                 print('Creating account for %s (%s)' % (username, email))
-                admin = User.objects.create_superuser(username=username, email=email, password=password)
+                admin = User.objects.create_superuser(
+                    username=username,
+                    email=email,
+                    password=password
+                )
                 admin.is_active = True
                 admin.is_admin = True
                 admin.save()
         else:
-            print('Admin accounts can only be initialized if no Accounts exist')
+            print(
+                'Admin accounts can only be initialized if no Accounts exist'
+            )

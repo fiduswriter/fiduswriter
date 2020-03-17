16bc4b4f954b08484f65f312667ced1892068421
Anand <a.sekar@alientt.com>
2020-03-17 12:19:07 +0530

    Added fn to window object to detect if user is online or offline.

--- fiduswriter/base/static/js/modules/common/network.js
+++ fiduswriter/base/static/js/modules/common/network.js
@@ -109,7 +109,7 @@ export const post = function(url, params={}, csrfToken=false) {
     // If the base ws of the app is disconnected we assume that user is offline.
     // Moreover if the ws readystate is not 0 we consider to check the connected status as 0 means ,
     // websocket is trying to establish a connection.
-    if (window.theApp !== undefined && window.theApp.ws !== undefined && window.theApp.ws.ws.readyState!=0 && !window.theApp.ws.connected) {
+    if (window.isOnline !== undefined && !window.isOnline()) {
         addAlert('error', gettext("You're offline now. Please try again after coming Online"))
         return Promise.reject(new Error('offline')).then(()=>{}, (error)=> {
             throw error

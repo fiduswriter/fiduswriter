5ccb2f4a0951cc1d4b16581a205f9d698b0a131b
Johannes Wilm <mail@johanneswilm.org>
2020-11-21 09:50:22 +0100

    retry => retry_run

--- ci/retry.bash
+++ ci/retry.bash
@@ -3,7 +3,7 @@
 # License: MIT
 # Copied from https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/travis_retry.bash
 # (Permalink: https://github.com/travis-ci/travis-build/blob/4e57ccad0d384bf48848d95c9ab19b039af32551/lib/travis/build/bash/travis_retry.bash)
-retry() {
+retry_run() {
   local result=0
   local count=1
   while [[ "${count}" -le 3 ]]; do

17f8833884913eb46c4a8a9b80b035b4ec5ff17f
Anand <a.sekar@alientt.com>
2020-11-26 15:24:49 +0530

    check protected range calculation only if main editor.

--- fiduswriter/document/static/js/modules/editor/menus/selection/model.js
+++ fiduswriter/document/static/js/modules/editor/menus/selection/model.js
@@ -54,24 +54,29 @@ export const selectionMenuModel = () => ({
                 mark => mark.type.name === 'comment'
             ),
             disabled: editor => {
-                const anchorDocPart = editor.currentView.state.selection.$anchor.node(2),
-                    headDocPart = editor.currentView.state.selection.$head.node(2)
+                if (editor.currentView === editor.view) {
+                    //  main editor
+                    const anchorDocPart = editor.currentView.state.selection.$anchor.node(2),
+                        headDocPart = editor.currentView.state.selection.$head.node(2)
 
-                // If the protection is of header/start type disable buttons only if it falls within the
-                // protected range
-                if(['start', 'header'].includes(anchorDocPart.attrs.locking) || ['start', 'header'].includes(headDocPart.attrs.locking)) {
-                    const protectedRanges = documentTemplatePluginKey.getState(editor.view.state).protectedRanges,
-                        start = editor.currentView.state.selection.from,
-                        end = editor.currentView.state.selection.to
-                    if (protectedRanges.find(({from, to}) => !(
-                        (start <= from && end <= from) ||
-                        (start >= to && end >= to)
-                    ))) {
-                        return true
+                    // If the protection is of header/start type disable buttons only if it falls within the
+                    // protected range
+                    if(['start', 'header'].includes(anchorDocPart.attrs.locking) || ['start', 'header'].includes(headDocPart.attrs.locking)) {
+                        const protectedRanges = documentTemplatePluginKey.getState(editor.view.state).protectedRanges,
+                            start = editor.currentView.state.selection.from,
+                            end = editor.currentView.state.selection.to
+                        if (protectedRanges.find(({from, to}) => !(
+                            (start <= from && end <= from) ||
+                            (start >= to && end >= to)
+                        ))) {
+                            return true
+                        }
                     }
+                    return anchorDocPart.attrs.locking === "fixed" || headDocPart.attrs.locking === "fixed"
+                } else {
+                    // footnote editor
+                    return false
                 }
-
-                return anchorDocPart.attrs.locking === "fixed" || headDocPart.attrs.locking === "fixed"
             },
             order: 1
         },
@@ -85,24 +90,30 @@ export const selectionMenuModel = () => ({
                 command(editor.currentView.state, tr => editor.currentView.dispatch(tr))
             },
             disabled: editor => {
-                const anchorDocPart = editor.currentView.state.selection.$anchor.node(2),
-                    headDocPart = editor.currentView.state.selection.$head.node(2)
+                if (editor.currentView === editor.view) {
+                    //  main editor
+                    const anchorDocPart = editor.currentView.state.selection.$anchor.node(2),
+                        headDocPart = editor.currentView.state.selection.$head.node(2)
 
-                if(['start', 'header'].includes(anchorDocPart.attrs.locking) || ['start', 'header'].includes(headDocPart.attrs.locking) ) {
-                    const protectedRanges = documentTemplatePluginKey.getState(editor.view.state).protectedRanges,
-                        start = editor.currentView.state.selection.from,
-                        end = editor.currentView.state.selection.to
-                    if (protectedRanges.find(({from, to}) => !(
-                        (start <= from && end <= from) ||
-                        (start >= to && end >= to)
-                    ))) {
-                        return true
+                    if(['start', 'header'].includes(anchorDocPart.attrs.locking) || ['start', 'header'].includes(headDocPart.attrs.locking) ) {
+                        const protectedRanges = documentTemplatePluginKey.getState(editor.view.state).protectedRanges,
+                            start = editor.currentView.state.selection.from,
+                            end = editor.currentView.state.selection.to
+                        if (protectedRanges.find(({from, to}) => !(
+                            (start <= from && end <= from) ||
+                            (start >= to && end >= to)
+                        ))) {
+                            return true
+                        }
                     }
-                }
 
-                return anchorDocPart.attrs.locking === "fixed" ||
-                    headDocPart.attrs.locking === "fixed" ||
-                    COMMENT_ONLY_ROLES.includes(editor.docInfo.access_rights)
+                    return anchorDocPart.attrs.locking === "fixed" ||
+                        headDocPart.attrs.locking === "fixed" ||
+                        COMMENT_ONLY_ROLES.includes(editor.docInfo.access_rights)
+                } else {
+                    // footnote editor
+                    return COMMENT_ONLY_ROLES.includes(editor.docInfo.access_rights)
+                }
             },
             hidden: editor => editor.currentView.state.selection.$anchor.depth < 2,
             selected: editor => !!editor.currentView.state.selection.$head.marks().some(

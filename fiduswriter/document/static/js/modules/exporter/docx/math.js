ec1f5c98cd65e968b190340813b33b988f93775e
Johannes Wilm <mail@johanneswilm.org>
2021-01-21 20:13:12 +0100

    mathml export fix

--- fiduswriter/document/static/js/modules/exporter/docx/math.js
+++ fiduswriter/document/static/js/modules/exporter/docx/math.js
@@ -49,6 +49,7 @@ export class DocxExporterMath {
             .replace(/&ApplyFunction;/g, '&#x2061;')
             .replace(/&PlusMinus;/g, '&#177;')
             .replace(/&times;/g, '&#215;')
+            .replace(/&x2061;/g, '&#x2061;') // Bug in mathlive 0.59. Has been fixed since.
     }
 
 

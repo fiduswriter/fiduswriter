d4dd6f390c1c49d3b3092bc06d03b315262665e9
arup patel <a.patel@alientt.com>
2021-04-06 18:19:31 +0530

    replace figure with options to original 4 options

--- fiduswriter/document/static/js/modules/editor/menus/figure/model.js
+++ fiduswriter/document/static/js/modules/editor/menus/figure/model.js
@@ -34,49 +34,29 @@ export const figureMenuModel = () => ({
 export const figureWidthMenuModel = () => ({
     content: [
         {
-            title: '10%',
+            title: '100%',
             type: 'action',
             order: 0,
-            value: '10',
+            value: '100',
             action: figureDialog => {
-                figureMenuAction("10", figureDialog)
+                figureMenuAction("100", figureDialog)
             },
             selected: false
         },
         {
-            title: '20%',
+            title: '75%',
             type: 'action',
-            order: 0,
-            value: '20',
+            order: 1,
+            value: '75',
             action: figureDialog => {
-                figureMenuAction("20", figureDialog)
-            },
-            selected: false
-        },
-        {
-            title: '30%',
-            type: 'action',
-            order: 0,
-            value: '30',
-            action: figureDialog => {
-                figureMenuAction("30", figureDialog)
-            },
-            selected: false
-        },
-        {
-            title: '40%',
-            type: 'action',
-            order: 0,
-            value: '40',
-            action: figureDialog => {
-                figureMenuAction("40", figureDialog)
+                figureMenuAction("75", figureDialog)
             },
             selected: false
         },
         {
             title: '50%',
             type: 'action',
-            order: 0,
+            order: 2,
             value: '50',
             action: figureDialog => {
                 figureMenuAction("50", figureDialog)
@@ -84,52 +64,12 @@ export const figureWidthMenuModel = () => ({
             selected: false
         },
         {
-            title: '60%',
+            title: '25%',
             type: 'action',
-            order: 0,
-            value: '60',
+            order: 3,
+            value: '25',
             action: figureDialog => {
-                figureMenuAction("60", figureDialog)
-            },
-            selected: false
-        },
-        {
-            title: '70%',
-            type: 'action',
-            order: 0,
-            value: '70',
-            action: figureDialog => {
-                figureMenuAction("70", figureDialog)
-            },
-            selected: false
-        },
-        {
-            title: '80%',
-            type: 'action',
-            order: 0,
-            value: '80',
-            action: figureDialog => {
-                figureMenuAction("80", figureDialog)
-            },
-            selected: false
-        },
-        {
-            title: '90%',
-            type: 'action',
-            order: 0,
-            value: '90',
-            action: figureDialog => {
-                figureMenuAction("90", figureDialog)
-            },
-            selected: false
-        },
-        {
-            title: '100%',
-            type: 'action',
-            order: 0,
-            value: '100',
-            action: figureDialog => {
-                figureMenuAction("100", figureDialog)
+                figureMenuAction("25", figureDialog)
             },
             selected: false
         }

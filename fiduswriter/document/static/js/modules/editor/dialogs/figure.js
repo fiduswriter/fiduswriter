15423fcc8073973a9f29c3981e3d7107fa073272
arup patel <a.patel@alientt.com>
2021-04-06 13:45:53 +0530

    added figure dimension in increments of 10. Added figure width menu in editor menu.

--- fiduswriter/document/static/js/modules/editor/dialogs/figure.js
+++ fiduswriter/document/static/js/modules/editor/dialogs/figure.js
@@ -324,24 +324,34 @@ export class FigureDialog {
             this.aligned = 'center'
         }
 
-        dropdownSelect(
-            this.dialog.dialogEl.querySelector('.figure-width'),
-            {
-                onChange: newValue => {
-                    this.width = newValue
-                    if (this.width == "100") {
-                        alignmentSelector.setValue('center')
-                        alignmentSelector.disable()
-                        this.aligned = 'center'
-                    } else {
-                        alignmentSelector.enable()
+        const figureWidthDOM = this.dialog.dialogEl.querySelector('.figure-width')
+        figureWidthDOM.style.width = "80%"
+        figureWidthDOM.firstElementChild.innerText = `${this.width}%`
+        figureWidthDOM.addEventListener(
+            'click',
+            event => {
+                const contentMenu = new ContentMenu({
+                    menu: this.editor.menu.figureWidthMenuModel,
+                    page: this,
+                    menuPos: {X: event.pageX, Y: event.pageY},
+                    onClose: () => {
+                        if (this.width == "100") {
+                            alignmentSelector.setValue('center')
+                            alignmentSelector.disable()
+                            this.aligned = 'center'
+                        }else{
+                            alignmentSelector.enable()
+                        }
                     }
-                },
-                width: '80%',
-                value: this.width
+                })
+                // const selectedMenuItem = contentMenu.menu.content.find(item => item.value === this.width)
+                // if (selectedMenuItem){
+                //     selectedMenuItem.selected = true
+                // }
+                contentMenu.open()
             }
         )
-
+        
         dropdownSelect(
             this.dialog.dialogEl.querySelector('.figure-category'),
             {
--- fiduswriter/document/static/js/modules/editor/menus/figure/model.js
+++ fiduswriter/document/static/js/modules/editor/menus/figure/model.js
@@ -1,4 +1,5 @@
 import {FigureDialog} from "../../dialogs"
+import {figureMenuAction} from "./utils"
 
 export const figureMenuModel = () => ({
     content: [
@@ -26,6 +27,111 @@ export const figureMenuModel = () => ({
                 editor.currentView.dispatch(tr)
             },
             disabled: editor => !(editor.currentView.state.selection.node?.type.name === 'figure') || editor.currentView.state.selection.node?.attrs.track?.find(track => track.type === 'deletion')
+        }
+    ]
+})
+
+export const figureWidthMenuModel = () => ({
+    content: [
+        {
+            title: '10%',
+            type: 'action',
+            order: 0,
+            value: '10',
+            action: figureDialog => {
+                figureMenuAction("10", figureDialog)
+            },
+            selected: false
+        },
+        {
+            title: '20%',
+            type: 'action',
+            order: 0,
+            value: '20',
+            action: figureDialog => {
+                figureMenuAction("20", figureDialog)
+            },
+            selected: false
+        },
+        {
+            title: '30%',
+            type: 'action',
+            order: 0,
+            value: '30',
+            action: figureDialog => {
+                figureMenuAction("30", figureDialog)
+            },
+            selected: false
+        },
+        {
+            title: '40%',
+            type: 'action',
+            order: 0,
+            value: '40',
+            action: figureDialog => {
+                figureMenuAction("40", figureDialog)
+            },
+            selected: false
+        },
+        {
+            title: '50%',
+            type: 'action',
+            order: 0,
+            value: '50',
+            action: figureDialog => {
+                figureMenuAction("50", figureDialog)
+            },
+            selected: false
+        },
+        {
+            title: '60%',
+            type: 'action',
+            order: 0,
+            value: '60',
+            action: figureDialog => {
+                figureMenuAction("60", figureDialog)
+            },
+            selected: false
+        },
+        {
+            title: '70%',
+            type: 'action',
+            order: 0,
+            value: '70',
+            action: figureDialog => {
+                figureMenuAction("70", figureDialog)
+            },
+            selected: false
+        },
+        {
+            title: '80%',
+            type: 'action',
+            order: 0,
+            value: '80',
+            action: figureDialog => {
+                figureMenuAction("80", figureDialog)
+            },
+            selected: false
+        },
+        {
+            title: '90%',
+            type: 'action',
+            order: 0,
+            value: '90',
+            action: figureDialog => {
+                figureMenuAction("90", figureDialog)
+            },
+            selected: false
+        },
+        {
+            title: '100%',
+            type: 'action',
+            order: 0,
+            value: '100',
+            action: figureDialog => {
+                figureMenuAction("100", figureDialog)
+            },
+            selected: false
         }
     ]
 })

296ea824aac6ddfea972161207d39a0161866b05
Johannes Wilm <mail@johanneswilm.org>
2021-07-10 09:38:06 +0200

    fix odt export: citations in footnotes with footnote citation style

--- fiduswriter/document/static/js/modules/exporter/odt/richtext.js
+++ fiduswriter/document/static/js/modules/exporter/odt/richtext.js
@@ -171,7 +171,7 @@ export class OdtExporterRichtext {
             } else {
                 cit = this.exporter.citations.pmCits.shift()
             }
-            if (options.citationType === 'note') {
+            if (options.citationType === 'note' && !options.inFootnote) {
                 // If the citations are in notes (footnotes), we need to
                 // put the contents of this citation in a footnote.
                 start += noSpaceTmp`

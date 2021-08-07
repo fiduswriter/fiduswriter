4a9e4c0fd4948cc94f4a2d8408473309e679043e
Johannes Wilm <mail@johanneswilm.org>
2021-08-07 13:31:42 +0200

    reestablish loginas link on user admin pages

--- fiduswriter/user/admin.py
+++ fiduswriter/user/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from . import models
-
-
-class UserAdmin(admin.ModelAdmin):
-    pass
 
 
 admin.site.register(models.User, UserAdmin)

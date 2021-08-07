c229c61d47bf8bdcd74c63e254507128ae497b97
Johannes Wilm <mail@johanneswilm.org>
2021-08-07 15:32:28 +0200

    add contacts to user admin page

--- fiduswriter/user/admin.py
+++ fiduswriter/user/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from . import models
+
+
+class UserAdmin(DefaultUserAdmin):
+    fieldsets = DefaultUserAdmin.fieldsets + (
+        (_("Connections"), {"fields": ("contacts",)}),
+    )
 
 
 admin.site.register(models.User, UserAdmin)

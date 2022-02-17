6766afeae9d392f0a1e9125d8d66e991e1ca89f7
Johannes Wilm <mail@johanneswilm.org>
2022-02-17 23:25:35 +0100

    add fix option to global lint command

--- fiduswriter/base/management/commands/lint.py
+++ fiduswriter/base/management/commands/lint.py
@@ -3,9 +3,19 @@ from django.core.management import call_command
 
 
 class Command(BaseCommand):
-    help = "Lint JavaScript and Python files"
+    help = "Lint all source files"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--fix",
+            action="store_true",
+            dest="fix",
+            default=False,
+            help="Whether to also attempt to fix files",
+        )
 
     def handle(self, *args, **options):
-        call_command("lint_js")
-        call_command("lint_css")
-        call_command("lint_py")
+        fix = options["fix"]
+        call_command("lint_js", fix=fix)
+        call_command("lint_css", fix=fix)
+        call_command("lint_py", fix=fix)

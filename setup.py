996d5196cf9697e7c36a32cfd208aaada650f465
Johannes Wilm <mail@johanneswilm.org>
2022-05-16 19:03:02 +0200

    fiduswriter-books ~= 3.10.11

--- setup.py
+++ setup.py
@@ -184,7 +184,7 @@ setuptools.setup(
     python_requires=">=3",
     install_requires=read("fiduswriter/requirements.txt").splitlines(),
     extras_require={
-        "books": "fiduswriter-books ~= 3.10.10",
+        "books": "fiduswriter-books ~= 3.10.11",
         "citation-api-import": "fiduswriter-citation-api-import ~= 3.10.2",
         "languagetool": "fiduswriter-languagetool ~= 3.10.5",
         "ojs": "fiduswriter-ojs ~= 3.10.4",

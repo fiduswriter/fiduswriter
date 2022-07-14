3fbe00e329e405fd82cc7c65224c487997d78940
Johannes Wilm <mail@johanneswilm.org>
2022-07-14 15:10:58 +0200

    fix linting

--- fiduswriter/base/webpack.config.template.js
+++ fiduswriter/base/webpack.config.template.js
@@ -9,8 +9,7 @@ const baseRule = {
     use: {
         loader: "babel-loader",
         options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-syntax-dynamic-import", "@babel/plugin-proposal-optional-chaining"]
+            presets: ["@babel/preset-env"]
         }
     }
 }

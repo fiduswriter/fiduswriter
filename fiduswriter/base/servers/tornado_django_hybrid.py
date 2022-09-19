c4dbff083b293562996124d01c4616c4e2cf025c
Johannes Wilm <mail@johanneswilm.org>
2022-09-19 18:31:11 +0200

    switch to asgi

--- fiduswriter/base/servers/tornado_django_hybrid.py
+++ fiduswriter/base/servers/tornado_django_hybrid.py
@@ -1,18 +1,17 @@
 from importlib import import_module
 
 from django.conf import settings
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
 
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.web import Application, FallbackHandler, StaticFileHandler
-from tornado.wsgi import WSGIContainer
 
 from base.handlers import DjangoStaticFilesHandler, HelloHandler, RobotsHandler
 
+from . import asgi
 
 def make_tornado_server():
-    wsgi_app = WSGIContainer(get_wsgi_application())
     tornado_url_list = [
         (
             r"/static/(.*)",
@@ -48,7 +47,7 @@ def make_tornado_server():
                     dict(app_name=app_name),
                 )
             ]
-    tornado_url_list += [(".*", FallbackHandler, dict(fallback=wsgi_app))]
+    tornado_url_list += [(".*", asgi.AsgiHandler, dict(asgi_app=get_asgi_application()))]
     tornado_app = Application(
         tornado_url_list,
         debug=settings.DEBUG,

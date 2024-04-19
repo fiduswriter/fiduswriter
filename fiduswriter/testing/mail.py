fe7210bed91999ecfe49c73811c0ad631684483c
Johannes Wilm <mail@johanneswilm.org>
2024-04-19 17:16:49 +0200

    separate email boxes for each test series

--- fiduswriter/testing/mail.py
+++ fiduswriter/testing/mail.py
@@ -2,10 +2,11 @@ import os
 import shelve
 
 from django.core.mail.backends.base import BaseEmailBackend
+from django.conf import settings
 
 # Source: https://github.com/jricardo27/channels_example/commit/637b689870c2f879286f30eeb0c3e1d65283d557#diff-180eb8ba4002fc012b871a9b0c0ba23b31727c34a20d8470421841e9e3bb637b
 
-MAIL_STORAGE = ".test_mail_storage"
+MAIL_STORAGE_BASE = ".test_mail_storage_"
 OUTBOX = "outbox"
 
 
@@ -13,20 +14,28 @@ class EmailBackend(BaseEmailBackend):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        if hasattr(settings, "MAIL_STORAGE_NAME"):
+            mail_storage = MAIL_STORAGE_BASE + settings.MAIL_STORAGE_NAME
+        else:
+            mail_storage = MAIL_STORAGE_BASE
         # Clean outbox.
         try:
-            os.remove(MAIL_STORAGE)
+            os.remove(mail_storage)
         except OSError:
             pass
 
     def send_messages(self, messages):
         """Redirect messages to the dummy outbox"""
-        msg_count = 0
+        if hasattr(settings, "MAIL_STORAGE_NAME"):
+            mail_storage = MAIL_STORAGE_BASE + settings.MAIL_STORAGE_NAME
+        else:
+            mail_storage = MAIL_STORAGE_BASE
 
+        msg_count = 0
         for message in messages:  # .message() triggers header validation
             message.message()
 
-            storage = shelve.open(MAIL_STORAGE)
+            storage = shelve.open(mail_storage)
             outbox = storage.get(OUTBOX, [])
             outbox.append(message)
             storage[OUTBOX] = outbox
@@ -37,14 +46,22 @@ class EmailBackend(BaseEmailBackend):
         return msg_count
 
 
-def get_outbox():
-    storage = shelve.open(MAIL_STORAGE)
+def get_outbox(mail_storage_name=None):
+    if mail_storage_name:
+        mail_storage = MAIL_STORAGE_BASE + mail_storage_name
+    else:
+        mail_storage = MAIL_STORAGE_BASE
+    storage = shelve.open(mail_storage)
     outbox = storage[OUTBOX]
     storage.close()
     return outbox
 
 
-def empty_outbox():
-    storage = shelve.open(MAIL_STORAGE)
+def empty_outbox(mail_storage_name=None):
+    if mail_storage_name:
+        mail_storage = MAIL_STORAGE_BASE + mail_storage_name
+    else:
+        mail_storage = MAIL_STORAGE_BASE
+    storage = shelve.open(mail_storage)
     storage[OUTBOX] = []
     storage.close()

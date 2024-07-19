74e9eba642cd811386c4f44deae05ee8e3e00627
Johannes Wilm <mail@johanneswilm.org>
2024-07-19 10:46:46 +0200

    testing: better check for theApp

--- fiduswriter/testing/selenium_helper.py
+++ fiduswriter/testing/selenium_helper.py
@@ -158,5 +158,5 @@ class SeleniumHelper(object):
         return super().tearDown()
 
     def leave_site(self, driver):
-        driver.execute_script("if (theApp) {theApp.page = null;}")
+        driver.execute_script("if (window.theApp) {window.theApp.page = null;}")
         driver.get("data:,")

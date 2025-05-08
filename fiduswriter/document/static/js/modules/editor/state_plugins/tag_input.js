85b9675b386ee65b0c52ceda743e1cea52c7459a
Johannes Wilm <mail@johanneswilm.org>
2025-05-08 09:37:16 +0200

    better selection around tags, part 1

--- fiduswriter/document/static/js/modules/editor/state_plugins/tag_input.js
+++ fiduswriter/document/static/js/modules/editor/state_plugins/tag_input.js
@@ -2,10 +2,17 @@ import {GapCursor} from "prosemirror-gapcursor"
 import {history, redo, undo} from "prosemirror-history"
 import {keymap} from "prosemirror-keymap"
 import {Schema} from "prosemirror-model"
-import {EditorState, Plugin, PluginKey, TextSelection} from "prosemirror-state"
+import {
+    EditorState,
+    NodeSelection,
+    Plugin,
+    PluginKey,
+    TextSelection
+} from "prosemirror-state"
 import {ReplaceStep} from "prosemirror-transform"
 import {Decoration, DecorationSet, EditorView} from "prosemirror-view"
 
+import {tags_part} from "../../schema/document/structure"
 import {addDeletedPartWidget} from "./document_template"
 
 const key = new PluginKey("tagInput")
@@ -235,7 +242,6 @@ export class TagsPartView {
         this.dom.classList.add("doc-part")
         this.dom.classList.add(`doc-${this.node.type.name}`)
         this.dom.classList.add(`doc-${this.node.attrs.id}`)
-        this.dom.contentEditable = false
         if (node.attrs.hidden) {
             this.dom.dataset.hidden = true
         }
@@ -259,26 +265,58 @@ export class TagsPartView {
     }
 
     stopEvent(event) {
+        console.log("stopEvent start", {event})
         if (["click", "mousedown"].includes(event.type)) {
             return false
         } else if (!this.tagInputView || this.node.attrs.locking === "fixed") {
             return false
         } else if (
-            event.type === "keydown" &&
+            event.type !== "keydown" ||
+            !this.tagInputView.hasFocus() ||
+            !["ArrowRight", "ArrowLeft"].includes(event.key)
+        ) {
+            // Not event keydown ArrowRight or ArrowLeft
+            console.log("not responsible", {
+                focus: this.tagInputView.hasFocus(),
+                state: this.tagInputView.state
+            })
+            return false
+        }
+        console.log("handle with stopEvent")
+
+        if (
             event.key === "ArrowRight" &&
             this.tagInputView.state.selection.from ===
                 this.tagInputView.state.doc.nodeSize - 3
         ) {
-            this.view.focus()
             const startPos = this.getPos(),
-                pos = startPos + this.view.state.doc.nodeAt(startPos).nodeSize,
-                $pos = this.view.state.doc.resolve(pos)
-            this.view.dispatch(
-                this.view.state.tr.setSelection(new TextSelection($pos))
+                pos = startPos + this.node.nodeSize
+
+            const {newPos, $newPos, selectionType} = findValidCaretPosition(
+                this.view.state,
+                pos,
+                1
             )
-            return false
+
+            if (!newPos) {
+                return false
+            }
+            this.view.focus()
+            let newSelection
+            if (selectionType === "gap") {
+                newSelection = GapCursor($newPos)
+            } else {
+                newSelection = TextSelection.create(
+                    this.view.state.doc,
+                    newPos,
+                    newPos
+                )
+            }
+            this.view.dispatch(this.view.state.tr.setSelection(newSelection))
+            event.preventDefault()
+            event.stopPropagation()
+            return true
         } else if (
-            event.type === "keydown" &&
             event.key === "ArrowLeft" &&
             this.tagInputView.state.selection.to === 1
         ) {
@@ -292,14 +330,36 @@ export class TagsPartView {
             this.view.dispatch(
                 this.view.state.tr.setSelection(new TextSelection($pos))
             )
-            return false
+            event.preventDefault()
+            event.stopPropagation()
+            return true
         }
-        return true
+        return false
     }
 
     ignoreMutation(_record) {
         return true
     }
+}
+
+const findValidCaretPosition = (state, pos, dir) => {
+    let selectionType
+    let newPos = pos
+    let $newPos
+    while (!selectionType) {
+        newPos += dir
+        if (newPos === 0 || newPos === state.doc.nodeSize) {
+            // Could not find any valid position
+            break
+        }
+        $newPos = state.doc.resolve(newPos)
+        if ($newPos.parent.inlineContent) {
+            selectionType = "text"
+        } else if (GapCursor.valid($newPos)) {
+            selectionType = "gap"
+        }
+    }
+    return {newPos, $newPos, selectionType}
 }
 
 export const tagInputPlugin = options =>
@@ -322,6 +382,199 @@ export const tagInputPlugin = options =>
             }
         },
         props: {
-            nodeViews: {}
+            nodeViews: {},
+            // Handle keyboard selection between tags
+            handleKeyDown(view, event) {
+                console.log("handleKeyDown start")
+                if (
+                    !["ArrowLeft", "ArrowRight"].includes(event.key) ||
+                    !view.hasFocus()
+                ) {
+                    return false
+                }
+
+                console.log("handle with handleKeyDown")
+
+                const $pos = view.state.selection.$from,
+                    pos = view.state.selection.from
+                let position, newPos, $newPos
+
+                if ($pos.parent.type.name === "tags_part") {
+                    position = "in"
+                } else if (
+                    (event.key === "ArrowRight" && $pos.nodeAfter) ||
+                    (event.key === "ArrowLeft" && $pos.nodeBefore)
+                ) {
+                    return
+                } else {
+                    const dir = event.key === "ArrowRight" ? 1 : -1
+                    const foundCaretPos = findValidCaretPosition(
+                        view.state,
+                        pos,
+                        dir
+                    )
+                    $newPos = foundCaretPos.$newPos
+                    if ($newPos?.parent.type.name === "tags_part") {
+                        position =
+                            event.key === "ArrowRight" ? "before" : "after"
+                        newPos = foundCaretPos.newPos
+                    }
+                }
+                if (!position) {
+                    // Caret is somewhere else than near a tags_part.
+                    return false
+                }
+                if (event.key === "ArrowRight") {
+                    // Moving right
+
+                    // If we're inside the tags part between tags
+                    if (position === "in") {
+                        // Check if there are any tags after the current position
+                        const offset = $pos.parentOffset
+
+                        const tagsPartNode = $pos.parent
+
+                        if (tagsPartNode.childCount > offset + 1) {
+                            const tr = view.state.tr.setSelection(
+                                NodeSelection.create(view.state.doc, pos + 1)
+                            )
+                            view.dispatch(tr)
+
+                            return true
+                        } else {
+                            // No more tags, move to the tag input field
+                            // Find the NodeView for this tags_part
+                            const {node} = view.domAtPos(pos + 1)
+                            const tagInput =
+                                node.nextElementSibling?.querySelector(
+                                    ".ProseMirror"
+                                )
+                            if (tagInput) {
+                                tagInput.focus()
+                                return true
+                            }
+                        }
+                    } else {
+                        // We're before the tags part, move to the first tag or input
+                        const tagsPartNode = $newPos.parent
+
+                        if (tagsPartNode.childCount > 0) {
+                            const tr = view.state.tr.setSelection(
+                                NodeSelection.create(view.state.doc, newPos)
+                            )
+                            view.dispatch(tr)
+                            return true
+                        } else {
+                            // No tags yet, focus the input field
+                            // Find the NodeView for this tags_part
+                            const {node} = view.domAtPos(newPos + 1)
+                            const tagInput =
+                                node.nextElementSibling?.querySelector(
+                                    ".ProseMirror"
+                                )
+                            if (tagInput) {
+                                tagInput.focus()
+                                return true
+                            }
+                        }
+                    }
+                } else {
+                    // Moving left
+
+                    // If we're inside the tags part between tags
+                    if (position === "in") {
+                        // Check if there are any tags after the current position
+                        const offset = $pos.parentOffset
+
+                        //const tagsPartNode = $pos.parent
+
+                        if (offset > 0) {
+                            const tr = view.state.tr.setSelection(
+                                NodeSelection.create(view.state.doc, pos - 1)
+                            )
+                            view.dispatch(tr)
+                            return true
+                        } else {
+                            // No more tags, move to the last previous viable caret position
+                            const {newPos, $newPos, selectionType} =
+                                findValidCaretPosition(view.state, pos, -1)
+                            if (!newPos) {
+                                return false
+                            }
+                            let newSelection
+                            if (selectionType === "gap") {
+                                newSelection = new GapCursor($newPos)
+                            } else {
+                                // text selection
+                                newSelection = TextSelection.create(
+                                    view.state.doc,
+                                    newPos,
+                                    newPos
+                                )
+                            }
+                            const tr = view.state.tr.setSelection(newSelection)
+                            view.dispatch(tr)
+                            return true
+                        }
+                    } else {
+                        // We're after the tags part, move to tag input field or alternatively last tag
+
+                        // Find the NodeView for this tags_part
+                        const {node} = view.domAtPos(newPos)
+                        const tagInput =
+                            node.nextElementSibling?.querySelector(
+                                ".ProseMirror"
+                            )
+                        if (tagInput) {
+                            tagInput.focus()
+                            return true
+                        }
+
+                        const tagsPartNode = $newPos.parent
+
+                        if (tagsPartNode.childCount > 0) {
+                            const tr = view.state.tr.setSelection(
+                                NodeSelection.create(view.state.doc, newPos - 1)
+                            )
+                            view.dispatch(tr)
+                            return true
+                        }
+                    }
+                }
+                return false
+            }
+        },
+        appendTransaction: (trs, _oldState, state) => {
+            console.log("appendTransaction start")
+            // If selection is not collapsed, don't do anything
+            if (state.selection.from !== state.selection.to) {
+                return
+            }
+
+            const selectionSet = trs.find(tr => tr.selectionSet)
+            if (selectionSet) {
+                console.log("handle with appendTransaction")
+                const $from = state.selection.$from
+
+                // Check if we're inside a tags_part
+                if ($from.parent.type.name === "tags_part") {
+                    if ($from.nodeAfter) {
+                        return state.tr.setSelection(
+                            NodeSelection.create(
+                                state.doc,
+                                state.selection.from
+                            )
+                        )
+                    } else if ($from.nodeBefore) {
+                        return state.tr.setSelection(
+                            NodeSelection.create(
+                                state.doc,
+                                state.selection.from - 1
+                            )
+                        )
+                    }
+                }
+            }
+            return
         }
     })

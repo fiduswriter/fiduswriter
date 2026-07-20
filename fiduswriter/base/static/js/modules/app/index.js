f3f396d47a7bd799677281623950a492b349e1b7
Johannes Wilm <mail@johanneswilm.org>
2026-07-20 13:47:08 +0200

    import user template editor loader

--- fiduswriter/base/static/js/modules/app/index.js
+++ fiduswriter/base/static/js/modules/app/index.js
@@ -9,29 +9,36 @@ import {djangoApiConnectors} from "../api_adapters/index.js"
  * <app>/static/js/plugins/<name>/ are discoverable as
  * ../../plugins/<name>/ from any module in the overlay.
  *
- * Currently all plugin modules are empty placeholder init.js files.
- * When real plugins are added, update the paths below (and ensure
- * the plugin directories export an index.js / init.js with the
- * expected `plugins` named export).
+ * Core plugins are imported explicitly so that optional apps (books,
+ * pandoc, website, etc.) do not break the bundle when they are not
+ * installed. The App filters plugins by settings.APPS.
  */
 
-// import {plugins as appPlugins} from "../../plugins/app"
-// import {plugins as bibPlugins} from "../../plugins/bibliography_overview/index.js"
-// import {plugins as citationDialogPlugins} from "../../plugins/citation_dialog/index.js"
-// import {plugins as editorPlugins} from "../../plugins/editor/index.js"
-// import {plugins as menuPlugins} from "../../plugins/menu"
+import * as baseAppPlugins from "../../plugins/app/init.js"
+import * as userTemplateManagerAppPlugins from "../../plugins/app/user_template_manager.js"
+import * as bibliographyOverviewPlugins from "../../plugins/bibliography_overview/init.js"
+import * as citationDialogPlugins from "../../plugins/citation_dialog/init.js"
+import * as editorPlugins from "../../plugins/editor/init.js"
+import * as menuPlugins from "../../plugins/menu/init.js"
+import * as userTemplateManagerMenuPlugins from "../../plugins/menu/user_template_manager.js"
 
-const appPlugins = []
-const bibPlugins = []
-const citationDialogPlugins = []
-const editorPlugins = []
-const menuPlugins = []
+const appPlugins = [
+    ["base", baseAppPlugins],
+    ["user_template_manager", userTemplateManagerAppPlugins]
+]
+const bibPlugins = [["bibliography", bibliographyOverviewPlugins]]
+const citationDialogPluginList = [["document", citationDialogPlugins]]
+const editorPluginList = [["document", editorPlugins]]
+const menuPluginList = [
+    ["menu", menuPlugins],
+    ["user_template_manager", userTemplateManagerMenuPlugins]
+]
 
 const theApp = new App(djangoApiConnectors, window.settings, {
     appPlugins,
-    menuPlugins,
-    editorPlugins,
-    citationDialogPlugins,
+    menuPlugins: menuPluginList,
+    editorPlugins: editorPluginList,
+    citationDialogPlugins: citationDialogPluginList,
     bibliographyOverviewPlugins: bibPlugins
 })
 theApp.init()

90f4b118299b36f531aea092c74f1b5e4b73a1ff
Johannes Wilm <mail@johanneswilm.org>
2026-07-25 12:40:44 +0200

    Revert "Use safe_click_element for export template file link in admin test"
    This reverts commit ea912b7f09c36b3cf247d994118ee96422775d80.

--- fiduswriter/document/tests/test_admin.py
+++ fiduswriter/document/tests/test_admin.py
@@ -291,7 +291,7 @@ class AdminTest(SeleniumHelper, ChannelsLiveServerTestCase):
             By.CSS_SELECTOR, ".export-template-file a"
         )
         et_file = export_template_link.get_attribute("href").split("/")[-1]
-        self.safe_click_element(self.driver, export_template_link)
+        export_template_link.click()
         self.wait_until_file_exists(
             os.path.join(self.download_dir, et_file), self.wait_time
         )

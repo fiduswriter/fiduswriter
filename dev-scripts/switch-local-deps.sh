87c1b90606ad9ad2d2a96e644454e955ca32b11a
Johannes Wilm <mail@johanneswilm.org>
2026-08-13 22:46:21 +0200

    extend local deps switching to all plugin package.json5 files and bump document dep

--- dev-scripts/switch-local-deps.sh
+++ dev-scripts/switch-local-deps.sh
@@ -57,7 +57,16 @@ MAIN_FILES=(
     "$REPO_ROOT/fiduswriter/document/package.json5"
     "$REPO_ROOT/fiduswriter/base/package.json5"
     "$REPO_ROOT/fiduswriter/book/package.json5"
+    "$REPO_ROOT/fiduswriter/citation_api_import/package.json5"
+    "$REPO_ROOT/fiduswriter/gitrepo_export/package.json5"
+    "$REPO_ROOT/fiduswriter/languagetool/package.json5"
+    "$REPO_ROOT/fiduswriter/llm/package.json5"
+    "$REPO_ROOT/fiduswriter/ojs/package.json5"
     "$REPO_ROOT/fiduswriter/pandoc/package.json5"
+    "$REPO_ROOT/fiduswriter/payment/package.json5"
+    "$REPO_ROOT/fiduswriter/phplist/package.json5"
+    "$REPO_ROOT/fiduswriter/tum/package.json5"
+    "$REPO_ROOT/fiduswriter/website/package.json5"
 )
 
 # Sibling packages that depend on other sibling packages.
